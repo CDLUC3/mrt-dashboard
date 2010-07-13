74dc2d2f086399bd13fa296a5489319c30219f7f
Erik Hetzner <egh@e6h.org>
2010-07-13 13:45:12 -0700

    add view to download & pass through file content

--- app/controllers/show_controller.rb
+++ app/controllers/show_controller.rb
@@ -4,4 +4,15 @@ class ShowController < ApplicationController
   def show
     @info = UriInfo.new("http://#{params[:id]}")
   end
+  
+  def view
+    @info = UriInfo.new("http://#{params[:id]}")
+    fileUri = @info.first(Mrt::Mrt.storedAt).to_uri
+    http = Mrt::HTTP.new(fileUri.scheme, fileUri.host, fileUri.port)
+    tmp_file = http.get_to_tempfile(fileUri.path)
+    send_file(tmp_file.path,
+              :filename => File.basename(@info.first(RDF::DC.identifier).value),
+              :type => @info.first(RDF::DC.format),
+              :disposition => 'inline')
+  end
 end
