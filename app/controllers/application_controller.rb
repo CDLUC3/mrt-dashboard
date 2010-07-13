628209d20ca772df64a5faea8dbced0ef052c52e
Erik Hetzner <egh@e6h.org>
2010-07-13 13:41:13 -0700

    fix spacing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
+
   def store
     return Mrt::Sparql::Store.new('http://localhost:8080/sparql/')
   end
-
 end
