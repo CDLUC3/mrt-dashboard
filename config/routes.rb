366e875ead2f28c0c9a2ad73aa851008a3d6b382
Erik Hetzner <erik.hetzner@ucop.edu>
2010-07-13 13:45:12 -0700

    add view to download & pass through file content

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,8 @@ MrtDashboard::Application.routes.draw do |map|
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
 
-  match('show/*id' => 'show#show')
+  match('show/view/*id' => 'show#view')
+  match('show/*id'      => 'show#show')
   root(:to => "dashboard#show")
 
   # See how all your routes lay out with "rake routes"
