44ec46832ee4f061947af94e0f9be9d060e92952
Erik Hetzner <erik.hetzner@ucop.edu>
2010-07-22 10:04:55 -0700

    add login via AD

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,24 @@
+class UserSessionsController < ApplicationController
+  before_filter :require_no_user, :only => [:login, :login_post]
+  before_filter :require_user,    :only => :logout
+  
+  def login
+    @user_session = UserSession.new
+  end
+  
+  def login_post
+    @user_session = UserSession.new(params[:user_session])
+    if @user_session.save
+      flash[:notice] = "Login successful!"
+      redirect_back_or_default "/"
+    else
+      render :action => :login
+    end
+  end
+  
+  def logout
+    current_user_session.destroy
+    flash[:notice] = "Logout successful!"
+    redirect_back_or_default '/'
+  end
+end
