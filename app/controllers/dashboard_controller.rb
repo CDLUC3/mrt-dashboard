8c5a65309aec9def3fd36bfd52cf51b50a449da9
Erik Hetzner <egh@e6h.org>
2010-08-06 13:24:10 -0700

    fixes for Mulgara

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,17 +3,17 @@ class DashboardController < ApplicationController
 
   Q = Mrt::Sparql::Q
   def show
-    @object_count  = store().select(Q.new("?s rdf:type mrt:Object", :select=>"COUNT(?s) as c")).map{|r| r['c']}[0].value
-    @version_count = store().select(Q.new("?s rdf:type mrt:StorageVersion", :select=>"COUNT(?s) as c")).map{|r| r['c']}[0].value
-    @file_count = store().select(Q.new("?s rdf:type mrt:StorageFile", :select=>"COUNT(?s) as c")).map{|r| r['c']}[0].value
+    @object_count  = store().select(Q.new("?s rdf:type mrt:Object")).size
+    @version_count = store().select(Q.new("?s rdf:type mrt:StorageVersion")).size
+    @file_count    = store().select(Q.new("?s rdf:type mrt:StorageFile")).size
     @total_size    = store().select(Q.new("?s dc:extent ?n")).map{|r| r['n'].value.to_i}.sum()
     q = Q.new("?so rdf:type mrt:StorageObject . 
                ?so mrt:isStoredObjectFor ?s . 
                ?s ?p ?o . 
                ?so dc:modified ?mod .",
-              :limit    =>100,
-              :select   =>"DISTINCT ?s",
-              :order_by =>"DESC(?mod)")
+              :limit    => 100,
+              :select   => "DISTINCT ?s ?mod",
+              :order_by => "DESC(?mod)")
     @recent_objects = store().select(q).map{|s| UriInfo.new(s['s']) }
   end
 
