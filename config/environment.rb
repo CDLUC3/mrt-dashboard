10d1c0ab85fdf0f10d71d4cd570d3fa76d9e6837
Erik Hetzner <erik.hetzner@ucop.edu>
2010-08-06 13:24:52 -0700

    explicitly include mrt/*

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,8 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 MrtDashboard::Application.initialize!
 require 'ostruct'
+require 'mrt/kernel'
+require 'mrt/sparql'
+require 'mrt/mrt'
+require 'mrt/http'
+#require 'nokogiri'
