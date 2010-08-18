b9119982c1c6b2704b1f13f203bbc8656e4cf06b
sfisher <scott.fisher@ucop.edu>
2010-08-17 17:35:51 -0700

    I can't get calls to the ingest service working, but this is the latest progress.  Some kind of ruby problem.

--- app/controllers/object_controller.rb
+++ app/controllers/object_controller.rb
@@ -1,3 +1,6 @@
+#require 'httpclient' #doesn't wor for some reason
+require 'rest_client'
+
 class ObjectController < ApplicationController
   before_filter :require_user
   before_filter :require_group
@@ -8,8 +11,21 @@ class ObjectController < ApplicationController
 
   def upload
     new_file = DataFile.save(params[:file], current_user.login)
-
-    HTTPClient.post('')
+=begin
+    res = HTTPClient.post(INGEST_SERVICE,
+      {
+        'file'              => File.new(new_file),
+        'type'              => params[:object_type],
+        'submitter'         => current_user.displayname,
+        'filename'          => params[:file].original_filename,
+        'profile'           => 'profileSFISHER', # XXX this needs to be changed
+        'creator'           => params[:author],
+        'title'             => params[:title],
+        'date'              => params[:date],
+        'localIdentifier'   => params[:local_id],
+        'responseForm'      => 'xml'
+      })
+=end
     render :text => new_file + params.inspect.to_s
 
   end
--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,12 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+Dir[File.dirname(__FILE__) + "/../vendor/*"].each do |path|
+  gem_name = File.basename(path.gsub(/-\d+.\d+.\d+$/, ''))
+  gem_path = path + "/lib/" + gem_name + ".rb"
+  require gem_path if File.exists? gem_path
+end
+
 # Initialize the rails application
 MrtDashboard::Application.initialize!
 require 'ostruct'
