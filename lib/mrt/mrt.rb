cb564c9413b5b480ebbd04e6de8575ab46f5a0b7
sfisher <scott.fisher@ucop.edu>
2010-09-03 16:08:53 -0700

    Some comments to make it easier to figure out where each namespace for RDF lives.

--- lib/mrt/mrt.rb
+++ lib/mrt/mrt.rb
@@ -4,4 +4,7 @@ module Mrt
   class Version < RDF::Vocabulary("http://uc3.cdlib.org/ontology/store/version#") ; end
   class Object < RDF::Vocabulary("http://uc3.cdlib.org/ontology/store/object#") ; end
   class ORE < RDF::Vocabulary("http://www.openarchives.org/ore/terms/") ; end
+  # RDF::DC = http://purl.org/dc/terms/identifier
+  # do uri_info#keys on object to see information about its keys
+  
 end
