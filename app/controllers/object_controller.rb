04a6eb38387304109a64108ca7fccb976763de47
sfisher <sfisher@cdl-sfisher-1>
2010-09-07 16:03:18 -0700

    Getting filenames showing up in sidebar and breaking correctly.

--- app/controllers/object_controller.rb
+++ app/controllers/object_controller.rb
@@ -11,6 +11,10 @@ class ObjectController < ApplicationController
   def index
     @stored_object = @object[Mrt::Object['hasStoredObject']].first
     @versions = @stored_object[Mrt::Object['versionSeq']].first.to_list
+    #files for current version
+    @files = @versions[@versions.length-1][Mrt::Version.hasFile]
+    @files.delete_if {|file| file[RDF::DC.identifier].to_s[0..10].eql?('system/mrt-')}
+    @files.sort! {|x,y| File.basename(x[RDF::DC.identifier].to_s.downcase) <=> File.basename(y[RDF::DC.identifier].to_s.downcase)}
   end
 
   def add
