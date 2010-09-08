ddde4b5bc4e6a936bc45f54e35da7d2e32a1d2f7
sfisher <scott.fisher@ucop.edu>
2010-09-08 15:38:01 -0700

    Getting accessgroups to work and getting SPARQL queries for stats for accessgroups.

--- app/controllers/collection_controller.rb
+++ app/controllers/collection_controller.rb
@@ -5,13 +5,37 @@ class CollectionController < ApplicationController
   Q = Mrt::Sparql::Q
 
   def index
-    @object_count = store().select(Q.new("?s rdf:type object:Object")).size
-    @version_count = store().select(Q.new("?s rdf:type version:Version")).size
-    @file_count = store().select(Q.new("?s rdf:type file:File")).size
-    @total_size = store().select(Q.new("?s dc:extent ?n")).map{|r| r['n'].value.to_i}.sum() #XXX this doesn't work right
+    @object_count = store().select(Q.new("?obj rdf:type object:Object .
+        ?obj object:isStoredObjectFor ?meta .
+        ?meta object:isInCollection <#{@group.sparql_id}>")).size
+
+    @version_count = store().select(Q.new("?vers rdf:type version:Version .
+        ?vers version:inObject ?obj .
+        ?obj object:isStoredObjectFor ?meta .
+        ?meta object:isInCollection <#{@group.sparql_id}>")).size
+
+    @file_count = store().select(Q.new("?file rdf:type file:File .
+        ?file file:inVersion ?vers .
+        ?vers version:inObject ?obj .
+        ?obj object:isStoredObjectFor ?meta .
+        ?meta object:isInCollection <#{@group.sparql_id}>")).size
+
+    q = Q.new("?obj rdf:type object:Object .
+        ?obj object:isStoredObjectFor ?meta .
+        ?meta object:isInCollection <#{@group.sparql_id}>",
+      :select => "?obj")
+
+    obs = store().select(q).map{|s| UriInfo.new(s['obj']) }
+
+    @total_size = 0
+    
+    obs.each{|ob| @total_size += ob[Mrt::Object.totalActualSize].to_s.to_i}
+
+    #@total_size = store().select(Q.new("?s dc:extent ?n")).map{|r| r['n'].value.to_i}.sum() #XXX this doesn't work right
     q = Q.new("?so rdf:type object:Object .
                ?so object:isStoredObjectFor ?s .
                ?s ?p ?o .
+               ?s object:isInCollection <#{@group.sparql_id}> .
                ?so dc:modified ?mod .",
                :limit => 100,
                :select => "DISTINCT ?s ?mod",
