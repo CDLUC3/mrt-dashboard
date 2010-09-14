e6401d12c61b0b9d69965ca088dc4bacb1e2affa
scott fisher <scott.fisher@ucop.edu>
2010-09-14 16:17:12 -0700

    Changes for Tracy.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   layout 'home', :except => ['choose_collection']
   before_filter :require_user,    :only => :choose_collection
-  before_filter :require_group_if_user, :except => :choose_collection
+  before_filter :require_group_if_user, :except => [:choose_collection, :credits]
 
   def choose_collection
     @groups = current_user.groups.sort{|x, y| x.description.downcase <=> y.description.downcase}
