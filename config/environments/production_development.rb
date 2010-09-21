88ab8cbcb53b3ec5f23bccd90f973b5578ab781c
scott fisher <scott.fisher@ucop.edu>
2010-09-21 12:24:10 -0700

    Making me commit before I can push. :-(

--- config/environments/production_development.rb
+++ config/environments/production_development.rb
@@ -1,9 +1,3 @@
-#this is because I couldn't get our patched library to load otherwise for some reason
-$:.unshift File.join(Rails.root, 'vendor','gems', 'net-ldap-0.1.1-patched', 'lib')
-require 'user_ldap'
-require 'group_ldap'
-require 'net/ldap'
-
 MrtDashboard::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
 
@@ -25,31 +19,16 @@ MrtDashboard::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
-  LDAP_USER = UserLdap::Server.new(
-       {:host             => "dp01.cdlib.org",
-         :port            => 1636,
-         :base            => 'ou=People,ou=uc3,dc=cdlib,dc=org',
-         :admin_user      => 'Directory Manager',
-         :admin_password  => 'XXXXXXXX',
-         :minter          => 'http://noid.cdlib.org/nd/noidu_p9'}
-    )
+  LDAP_ADMIN_PASSWORD = "XXXXXXXX"
+  LDAP_ADMIN_USER     = "cn=Directory Manager"
+  LDAP_GROUP_BASE     = "ou=mrt-groups,ou=uc3,dc=cdlib,dc=org"
+  LDAP_HOST           = "dp01.cdlib.org"
+  LDAP_PORT           = 1636
+  LDAP_USER_BASE      = "ou=People,ou=uc3,dc=cdlib,dc=org"
+  LDAP_ARK_MINTER_URL = "http://noid.cdlib.org/nd/noidu_p9"
 
-  LDAP_GROUP = GroupLdap::Server.new(
-       {:host             => "dp01.cdlib.org",
-         :port            => 1636,
-         :base            => 'ou=mrt-groups,ou=uc3,dc=cdlib,dc=org',
-         :admin_user      => 'Directory Manager',
-         :admin_password  => 'XXXXXXXX',
-         :minter          => 'http://noid.cdlib.org/nd/noidu_p9'}
-    )
-
-  INGEST_SERVICE = 'http://.cdlib.org:33121/poster/submit/'
-
-  SPARQL_ENDPOINT = "http://dp01.cdlib.org:8080/sparql/"
-
-  RDF_ARK_URI = "http://ark.cdlib.org/"
-  RDF_COLLECTION_URI = "http://uc3.cdlib.org/collection/"
-
-  STORE_URI = "http://badger.cdlib.org:35121/content/10/"
-
+  INGEST_SERVICE      = 'http://uc3.cdlib.org:33121/poster/submit/'
+  SPARQL_ENDPOINT     = "http://dp01.cdlib.org:8080/sparql/"
+  RDF_ARK_URI         = "http://ark.cdlib.org/"
+  RDF_COLLECTION_URI  = "http://uc3.cdlib.org/collection/"
 end
