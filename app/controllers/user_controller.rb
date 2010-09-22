28264f47d890cdbd25bde98997dbef73702d37cb
scott fisher <scott.fisher@ucop.edu>
2010-09-21 17:01:55 -0700

    Changing this controller to connect with LDAP correctly after some changes erik made.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,7 +5,7 @@ class UserController < ApplicationController
     #this could be made more efficient and wonderful, but I think that we may be going
     #to a different authentication service soon, so it will all need rewriting, anyway, might as well wait.
     require_group_if_user if !session[:group].nil? or !params[:group].nil? #get group info if it's there
-    @ldap_user = LDAP_USER.fetch(current_user.login) #uncached from LDAP, so always current
+    @ldap_user = User::LDAP.fetch(current_user.login) #uncached from LDAP, so always current
     #update if this is submitted with appropriate information
     @error_fields = []
     @display_text = ''
@@ -28,13 +28,13 @@ class UserController < ApplicationController
         end
       else
         ['givenname', 'sn', 'mail', 'tzregion', 'telephonenumber'].each do |i|
-          LDAP_USER.replace_attribute(current_user.login, i, params[i])
+          User::LDAP.replace_attribute(current_user.login, i, params[i])
         end
         #special fields
-        LDAP_USER.replace_attribute(current_user.login, 'cn', "#{params['givenname']} #{params['sn']}")
-        LDAP_USER.replace_attribute(current_user.login, 'displayname', "#{params['givenname']} #{params['sn']}")
+        User::LDAP.replace_attribute(current_user.login, 'cn', "#{params['givenname']} #{params['sn']}")
+        User::LDAP.replace_attribute(current_user.login, 'displayname', "#{params['givenname']} #{params['sn']}")
         if !params['userpassword'].eql?('!unchanged') then
-          LDAP_USER.replace_attribute(current_user.login, 'userpassword', params['userpassword'])
+          User::LDAP.replace_attribute(current_user.login, 'userpassword', params['userpassword'])
         end
         @display_text = "Your profile has been updated."
       end
