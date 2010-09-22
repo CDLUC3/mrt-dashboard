2871db579f82afb066a78560db843b851143e552
scott fisher <scott.fisher@ucop.edu>
2010-09-22 15:52:03 -0700

    Removing old comment that's not relevant anymore.

--- lib/institution_ldap.rb
+++ lib/institution_ldap.rb
@@ -17,7 +17,6 @@ module InstitutionLdap
     end
 
     def institutions
-      #these are the permission subgroups so they need to be parsed back up a level
       inst = @admin_ldap.search(:base => @base)
       inst.map{|i| i[:o][0]}.delete_if{|i| i.eql?('institutions')}.sort{|x,y| x.downcase <=> y.downcase}
     end
