48a806ca7d9c13e966f7a0228cb959791cdc5ac9
Erik Hetzner <egh@e6h.org>
2010-10-04 12:50:30 -0700

    fix term tokenizing to allow searching for arks, dates

--- app/controllers/collection_controller.rb
+++ app/controllers/collection_controller.rb
@@ -43,7 +43,7 @@ class CollectionController < ApplicationController
     pg_start = (@page -1) * @page_size
     pg_end = @page * @page_size - 1
 
-    terms = no_inject(params[:terms]).downcase().split(/\s+/)
+    terms = no_inject(params[:terms]).downcase().split(/[\s:\/_-]+/)
     terms_q = terms.map {|term| "<http://4store.org/fulltext#token> \"#{term}\"" }.join("; ")
 
     q = Q.new("?s a ore:Aggregation ;
