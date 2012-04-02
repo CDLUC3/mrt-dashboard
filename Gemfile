c1e102578eca3132f9c86e2dc14454436f33d1b3
Erik Hetzner <egh@e6h.org>
2012-04-02 15:27:19 -0700

    implement total_size with solr

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+gem 'rails', '3.0.12'
+gem "exception_notification", 
+    :git => "git://github.com/rails/exception_notification.git"
+gem "json", "> 1.5.0"
+gem "net-ldap", "0.1.2", 
+    :git=>"git://github.com/RoryO/ruby-net-ldap.git", 
+    :ref=>"74e994eed11a011cb73311e164a65a7dd7a62c01"
+gem "nokogiri"
+gem "rdf"
+gem "rest-client"
+gem "ruby-debug"
+gem "unicorn", "3.1.0"
+gem "will_paginate", "3.0.pre2"
+gem "rack-cache"
+gem "unicode"
+gem "orchard"
+gem "mrt-ingest", "0.0.2"
+gem "rsolr", "1.0.6"
+gem "rsolr-ext"
---
+++ app/models/group.rb
@@ -0,0 +1,118 @@
+# this all probably needs to be refactored eventually
+class Group
+  RSOLR = RSolr.connect(:url => SOLR_SERVER)
+
+  LDAP = GroupLdap::Server.
+    new({ :host            => LDAP_CONFIG["host"],
+          :port            => LDAP_CONFIG["port"],
+          :base            => LDAP_CONFIG["group_base"],
+          :admin_user      => LDAP_CONFIG["admin_user"],
+          :admin_password  => LDAP_CONFIG["admin_password"],
+          :minter          => LDAP_CONFIG["ark_minter_url"]})
+
+  Q = Mrt::Sparql::Q
+  STORE = Mrt::Sparql::Store.new(SPARQL_ENDPOINT)
+
+  attr_accessor :id, :submission_profile, :ark_id, :owner, :description
+
+  def initialize
+
+  end
+
+  def self.find_all
+    LDAP.find_all
+  end
+
+  def self.find_users(grp_id)
+    LDAP.find_users(grp_id)
+  end
+
+  def self.find(id)
+    #fetch by groupid, but otherwise, fall back to arkid
+    ldap_group = begin
+                   Group::LDAP.fetch(id)
+                 rescue LdapMixin::LdapException => ex
+                   Group::LDAP.fetch_by_ark_id(id)
+                 end
+    return self.make_from_ldap(ldap_group)
+  end
+  
+  # permissions are returned as an array like ['read','write'], maybe more in the future
+  def permission(userid)
+    Group::LDAP.get_user_permissions(userid, self.id, User::LDAP)
+  end
+
+  def sparql_id
+    return "http://ark.cdlib.org/#{self.ark_id}"
+  end
+
+  def object_count
+    response = RSOLR.get('select', 
+                         :params => {:q => "type:object and memberOf:\"#{self.ark_id}\"", 
+                         :fl=>""})
+    return response['response']['numFound'].to_i
+  end
+
+  def version_count
+    response = RSOLR.get('select', 
+                         :params => {:q => "type:version and memberOf:\"#{self.ark_id}\"",
+                         :fl=>""})
+    return response['response']['numFound'].to_i
+  end
+
+  def file_count
+    q = Q.new("?obj base:isInCollection <#{self.sparql_id}> .
+               ?vers version:inObject ?obj ;
+                     version:hasFile ?file .",
+              :select=>"(count(?file) as c)")
+    return STORE.select(q)[0]["c"].value.to_i
+  end
+
+  def total_size
+    response = RSOLR.get('select', 
+                         :params => {
+                           :q    => "type:object and memberOf:\"#{self.ark_id}\"",
+                           :fl   => "totalActualSize",
+                           :rows => nil });
+    size =  0
+    response["response"]["docs"].each {|d| size += d["totalActualSize"] }
+    return size
+  end
+
+  #get all groups and email addresses of members, this is a stopgap for our own use
+  def self.show_emails
+    out_str = ''
+    Group.find_all.each do |grp|
+      out_str << "#{grp['ou'][0]}\r\n"
+      Group.find_users(grp['ou'][0]).each do |usr|
+        u = User::LDAP.fetch(usr)
+        out_str << "#{u[:mail][0]}\r\n"
+      end
+      out_str << "\r\n"
+    end
+    out_str
+  end
+
+  private
+
+  def self.make_from_ldap(ldap_group)
+    g = self.new
+    g.id                 = simplify_single_value(ldap_group, 'ou')
+    g.submission_profile = simplify_single_value(ldap_group, 'submissionprofile')
+    g.ark_id             = simplify_single_value(ldap_group, 'arkid')
+    g.owner              = simplify_single_value(ldap_group, 'owner')
+    g.description        = simplify_single_value(ldap_group, 'description')
+    return g
+  end
+
+  # this may belong to some ldap base class at some point
+  def self.simplify_single_value(record, field)
+    return nil if record[field].nil? or record[field][0].nil? or record[field][0].length < 1
+    return record[field][0]
+  end
+
+  def self.simplify_multiple_value(record, field)
+    return [] if record[field].nil? or record[field][0].nil? or record[field][0].length < 1
+    return record[field]
+  end
+end
