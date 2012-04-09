0bc577c6b4722f72f01ca62902b541e1802506b0
Erik Hetzner <egh@e6h.org>
2012-04-09 14:47:38 -0700

    use DateTime rather than Time; print times in UTC

---
+++ config/unicorn.rb
@@ -0,0 +1,20 @@
+require 'socket'
+
+rails_env = ENV['RAILS_ENV'] || 'production'
+
+# 16 workers and 1 master
+worker_processes 16
+
+# Load rails+github.git into the master before forking workers
+# for super-fast worker spawn times
+preload_app true
+
+pid File.join(Dir.pwd, "log", "unicorn.pid")
+
+# timeout is long because we upload files
+# switch to nginx to fix
+timeout 3000
+
+listen "#{Socket.gethostname}:26181"
+
+logger Logger.new(File.join(Dir.pwd, "log", "unicorn.log"))
