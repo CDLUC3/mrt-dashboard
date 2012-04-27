fbfe1676b3ecbfb025e429b2f13a23f2b68b14b1
mark reyes <mark.reyes@ucop.edu>
2012-04-27 15:46:17 -0700

    support for EZID passthru

--- config/environments/stage_development.rb
+++ config/environments/stage_development.rb
@@ -8,6 +8,7 @@ MrtDashboard::Application.configure do
   config.whiny_nils                        = true
   
   INGEST_SERVICE      = 'http://dp01.cdlib.org:33121/poster/submit/'
+  MINT_SERVICE      = 'http://dp01.cdlib.org:33121/request-identifier'
   SPARQL_ENDPOINT     = "http://dp01.cdlib.org:38082/sparql/"
   RDF_ARK_URI         = "http://ark.cdlib.org/"
   RDF_COLLECTION_URI  = "http://uc3.cdlib.org/collection/"
