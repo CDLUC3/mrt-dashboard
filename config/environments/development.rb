537e6dfda99bf7002cdf91f5b19824f872571f2d
Mark Reyes <mark.reyes@ucop.edu>
2012-04-27 15:46:17 -0700

    support for EZID passthru

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,6 +10,7 @@ MrtDashboard::Application.configure do
 
   INGEST_SERVICE      = 'http://badger.cdlib.org:33121/poster/submit/'
   INGEST_SERVICE_UPDATE   = 'http://badger.cdlib.org:33121/poster/update/'
+  MINT_SERVICE        = 'http://badger.cdlib.org:33121/request-identifier'
   RDF_ARK_URI         = "http://ark.cdlib.org/"
   RDF_COLLECTION_URI  = "http://uc3.cdlib.org/collection/"
   SPARQL_ENDPOINT     = "http://badger.cdlib.org:8082/sparql/"
