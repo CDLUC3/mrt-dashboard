8daa9c1b2ca37eec9ec70ba769aa2f8b74512df4
marisa strong <mxs4ever@gmail.com>
2012-05-11 16:04:38 -0700

    Restrict download functionality of object and files to only if user has download permission.  Includes Do not display the download button for object or versions,  do not display links to files for download.

---
+++ config/environments/development.rb
@@ -0,0 +1,17 @@
+MrtDashboard::Application.configure do
+  config.action_controller.perform_caching   = false
+  config.action_mailer.raise_delivery_errors = false
+  config.action_view.debug_rjs               = true
+  config.active_support.deprecation          = :log
+  config.autoload_paths                     += %W(#{config.root}/lib)
+  config.cache_classes                       = false
+  config.consider_all_requests_local         = true
+  config.whiny_nils                          = true
+
+  INGEST_SERVICE      = 'http://badger.cdlib.org:33121/poster/submit/'
+  INGEST_SERVICE_UPDATE   = 'http://badger.cdlib.org:33121/poster/update/'
+  N2T_URI             = "http://n2t-wf.cdlib.org/"
+  RDF_ARK_URI         = "http://ark.cdlib.org/"
+  RDF_COLLECTION_URI  = "http://uc3.cdlib.org/collection/"
+  SPARQL_ENDPOINT     = "http://badger.cdlib.org:8082/sparql/"
+end
---
+++ config/environments/stage.rb
@@ -0,0 +1,21 @@
+MrtDashboard::Application.configure do
+  config.action_controller.perform_caching = true
+  config.autoload_paths                   += %W(#{config.root}/lib)
+  config.cache_classes                     = true
+  config.consider_all_requests_local       = false
+  config.i18n.fallbacks                    = true
+  config.serve_static_assets               = true
+  
+  INGEST_SERVICE      = 'http://dp01.cdlib.org:33121/poster/submit/'
+  N2T_URI             = "http://n2t.net/"
+  SPARQL_ENDPOINT     = "http://dp01.cdlib.org:38082/sparql/"
+  RDF_ARK_URI         = "http://ark.cdlib.org/"
+  RDF_COLLECTION_URI  = "http://uc3.cdlib.org/collection/"
+end
+
+require 'exception_notifier'
+MrtDashboard::Application.config.middleware.use ExceptionNotifier,
+  :email_prefix => "[Merritt UI] ",
+  :sender_address => "\"notifier\" <no-reply@#{Socket.gethostname}>",
+  :exception_recipients => 
+    %w{erik.hetzner@ucop.edu scott.fisher@ucop.edu perry.willett@ucop.edu}
