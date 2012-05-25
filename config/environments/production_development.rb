2b8ce1f9a2bd9f76e7867b3d995487d8098a1140
sfisher <scott.fisher@ucop.edu>
2012-05-25 13:36:34 -0700

    These help files should use the partials, not inline content.

---
+++ config/environments/production_development.rb
@@ -0,0 +1,16 @@
+MrtDashboard::Application.configure do
+  config.action_controller.perform_caching   = false
+  config.action_mailer.raise_delivery_errors = false
+  config.action_view.debug_rjs               = false
+  config.active_support.deprecation          = :log
+  config.autoload_paths                     += %W(#{config.root}/lib)
+  config.cache_classes                       = true
+  config.consider_all_requests_local         = false
+  config.whiny_nils                          = true
+
+  INGEST_SERVICE      = 'http://uc3.cdlib.org:33121/poster/submit/'
+  MINT_SERVICE      = 'http://uc3.cdlib.org:33121/request-identifier'
+  SPARQL_ENDPOINT     = "http://inventory.cdlib.org:8082/sparql/"
+  RDF_ARK_URI         = "http://ark.cdlib.org/"
+  RDF_COLLECTION_URI  = "http://uc3.cdlib.org/collection/"
+end
