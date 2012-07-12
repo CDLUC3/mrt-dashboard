4b6b6e3106799ed8b669165f20784c7c132d2095
marisa strong <mxs4ever@gmail.com>
2012-07-11 18:15:12 -0700

    slight tweeks for version permalink server name.  added dua email feature

--- app/controllers/dua_controller.rb
+++ app/controllers/dua_controller.rb
@@ -30,12 +30,12 @@ class DuaController < ApplicationController
       
       to_email = params[:email] + "," +  # need to obtain owner of collection email
                    APP_CONFIG['feedback_email_to'].join(", ")
-      #ContactMailer.feedback_email(params[:email],
-      #          { 'title``'  => @title,
-      #            'to_email'        => to_email,
-      #            'name'            => params[:name],
-      #            'body'            => params[:body]}).deliver
-      
+      DuaMailer.feedback_email(params[:email],
+               { 'title``'   => @dua_hash["Title"],
+                  'to_email' => to_email,
+                  'name'     => params[:name],
+                  'body'     => @dua_hash["Terms"]}).deliver
+       
       #user accepted DUA, go ahead and process file/object/version download
       session[:collection_acceptance][@group.id] = true
        # return to where user came from 
--- config/environments/stage.rb
+++ config/environments/stage.rb
@@ -7,7 +7,8 @@ MrtDashboard::Application.configure do
   config.serve_static_assets               = true
   
   INGEST_SERVICE      = 'http://dp01.cdlib.org:33121/poster/submit/'
-  MINT_SERVICE      = 'http://dp01.cdlib.org:33121/ingest/request-identifier'
+  MERRITT_SERVER      = 'http://merritt-stage.cdlib.org'
+  MINT_SERVICE        = 'http://dp01.cdlib.org:33121/ingest/request-identifier'
   N2T_URI             = "http://n2t-wf.cdlib.org/"
   SPARQL_ENDPOINT     = "http://dp01.cdlib.org:38082/sparql/"
   RDF_ARK_URI         = "http://ark.cdlib.org/"
