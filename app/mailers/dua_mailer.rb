42b9f452b567521b77d00d0b2714a0338a2bff2a
Marisa Strong <marisa.strong@ucop.edu>
2012-07-17 12:14:38 -0700

    remove debug stmt

--- app/mailers/dua_mailer.rb
+++ app/mailers/dua_mailer.rb
@@ -3,7 +3,6 @@ class DuaMailer < ActionMailer::Base
   default :from => APP_CONFIG['dua_email_from']
 
   def dua_email(dua_hsh, hsh)
-    debugger
     @hsh = {'body' => ''}.merge(hsh)
      mail( :to            => "#{@hsh['to_email']}",
            :subject       => "Merritt DUA acceptance: " + dua_hsh['Title'],
