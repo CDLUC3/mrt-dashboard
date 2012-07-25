b26bc192ce0f30352217db65738f3df1631b53cd
Marisa Strong <marisa.strong@ucop.edu>
2012-07-24 17:45:08 -0700

    testing processing EOL characters in the DUA

--- app/models/dua.rb
+++ app/models/dua.rb
@@ -6,7 +6,7 @@ class Dua
     # regex to capture match of name:value and put them into a hash
     rx = /(\w+)\s*:\s*(.+)/
     a = []
-    File.open(dua_file.path) do |f| 
+    File.open(dua_file.path, "rb") do |f| 
       while line = f.gets
         if !rx.match(line).nil?
           a << $~.captures
