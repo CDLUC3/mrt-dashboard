ed2a591367550ffa978e77c870945c394698a0f4
Erik Hetzner <egh@e6h.org>
2012-08-09 12:00:54 -0700

    add size to each atom entry, mediatype & size to each object in the entry

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,90 @@
+class AdminController < ApplicationController
+  before_filter :require_user
+  before_filter :group_optional
+
+  def create_user
+    @ldap_user = {'tzregion' => 'America/Los_Angeles'}
+    @error_fields = []
+    @display_text = ''
+    if !params[:givenname].nil? then
+      params.each_pair{|k,v| @ldap_user[k] = v } #stuck updated info in this hash so they don't have to retype
+      @required = {'givenname' => 'First name', 'sn' => 'Last name', 'uid' => 'User Id',
+                  'userpassword' => 'Password', 'repeatuserpassword' => 'Repeat Password',
+                  'mail' => 'Email'}
+      @required.each_key do |key|
+        if params[key].blank? then
+          @error_fields.push(key)
+        end
+      end
+      if @error_fields.length > 0 or !params[:userpassword].eql?(params[:repeatuserpassword]) then
+        if @error_fields.length > 0 then
+          @display_text += "The following items must be filled in: #{@error_fields.map{|i| @required[i]}.join(', ' )}."
+        end
+        if !params[:userpassword].eql?(params[:repeatuserpassword]) then
+          @display_text += " Your password and repeated password do not match."
+        end
+      else
+        User::LDAP.add(params[:uid], params[:userpassword], params[:givenname],
+                params[:sn], params[:mail])
+        ['tzregion', 'telephonenumber', 'institution'].each do |i|
+          User::LDAP.replace_attribute(params[:uid], i, params[i])
+        end
+        @display_text = "This user profile has been created."
+        @ldap_user = User::LDAP.fetch(params[:uid])
+      end
+    end
+  end
+
+  def create_group
+    @ldap_group = {}
+    @error_fields = []
+    @display_text = ''
+    if !params[:ou].nil? then
+      params.each_pair{|k,v| @ldap_group[k] = v } #stuck updated info in this hash so they don't have to retype
+      @required = {'ou' => 'collection ID', 'description' => 'description',
+            'submissionprofile' => 'Ingest Profile ID'}
+      @required.each_key do |key|
+        if params[key].blank? then
+          @error_fields.push(key)
+        end
+      end
+      if @error_fields.length > 0 then
+        @display_text += "The following items must be filled in: #{@error_fields.map{|i| @required[i]}.join(', ' )}."
+      else
+        Group::LDAP.add(params[:ou], params[:description], ['read', 'write'], ['merrittClass'])
+        Group::LDAP.replace_attribute(params[:ou], 'submissionprofile', params['submissionprofile'])
+        @display_text = "This collection has been created."
+        @ldap_group = Group::LDAP.fetch(params[:ou])
+      end
+    end
+  end
+
+  def add_user_to_group
+    @ldap = {}
+    @m_grps = Group.find_all.map{|i| [ i['description'][0], i['ou'][0] ]}
+    @m_usrs = User.find_all.map{|i| [ i['cn'][0], i['uid'][0] ]}
+    @perms = []
+
+    if params[:submitted].eql?('true') then
+      @ldap[:uid] = params[:uid]
+      @ldap[:ou] = params[:ou]
+
+      ['read', 'write'].each do |perm|
+        if params[:permissions].nil? or !params[:permissions].include?(perm) then
+          Group::LDAP.unset_user_permission(params[:uid], params[:ou], User::LDAP, perm)
+        else
+          Group::LDAP.set_user_permission(params[:uid], params[:ou], User::LDAP, perm)
+        end
+      end
+      #get permissions from LDAP
+      usr = User::LDAP.fetch(params[:uid])
+      grp = Group.find(params[:ou])
+      @perms = grp.permission(params[:uid])
+      @display_text = "The user permissions were set as shown below.  The user is likely to need " +
+       "their email address (#{usr[:mail][0]}) added or removed from the ingest profile (#{grp.submission_profile}) in the ingest service." +
+       " If it is not modified they may not receive appropriate emails from the ingest service."
+    else
+      #nothing checked
+    end
+  end
+end
---
+++ app/views/object/recent.atom.builder
@@ -0,0 +1,83 @@
+# -*- mode: ruby -*-
+
+xml.tag!('feed', :xmlns => "http://www.w3.org/2005/Atom",
+                 "xmlns:dct" => "http://purl.org/dc/terms/") do 
+  xml.tag!("link", 
+           "href" => "/object/recent.atom?collection=#{@collection_ark}&page=#{@objects.current_page}",
+           "rel"  => "self", 
+           "type" => "application/atom+xml")
+  xml.tag!("link", 
+           "href" => "/object/recent.atom?collection=#{@collection_ark}&page=1",
+           "rel"  => "first", 
+           "type" => "application/atom+xml")
+  xml.tag!("link", 
+           "href" => "/object/recent.atom?collection=#{@collection_ark}&page=#{@objects.total_pages}",
+           "rel"  => "last",
+           "type" => "application/atom+xml")
+  if @objects.next_page
+    xml.tag!("link", 
+           "href" => "/object/recent.atom?collection=#{@collection_ark}&page=#{@objects.next_page}",
+             "rel"  => "next", 
+             "type" => "application/atom+xml")
+  end
+  if @objects.previous_page
+    xml.tag!("link", 
+             "href" => "/object/recent.atom?collection=#{@collection_ark}&page=#{@objects.previous_page}",
+             "rel"  => "previous", 
+             "type" => "application/atom+xml")
+  end
+  xml.tag!("id", "urn:uuid:8dd71209-616a-4723-bfc1-b46572499932")
+  xml.tag!("title", "Recent objects")
+  if @objects[0] then
+    xml.tag!("updated", w3cdtf(@objects[0].modified))
+  else
+    xml.tag!("updated", w3cdtf(Time.now))
+  end
+  xml.tag!("author") do
+    xml.tag!("name", "California Digital Library")
+    xml.tag!("email", "uc3@ucop.edu")
+  end
+  xml.tag!("generator", "UC3 Dashboard", "version" => "0", "uri" => "http://www.cdlib.org/")
+  @objects.each do |obj|
+    xml.tag!("entry") do
+      ark = obj.is_stored_object_for.to_s
+      xml.tag!("id", ark)
+      xml.tag!("link", 
+               "rel"  => "alternate",
+               "type" => "application/zip",
+               "href" => url_for(:controller => 'object', 
+                                 :action     => 'download',
+                                 :group      => @collection_ark,
+                                 :object     => clean_id(ark)))
+
+      xml.tag!("dct:extent", "#{obj.size}")
+      if (!obj.local_identifier.nil?) then
+        local_id = obj.local_identifier.value
+        if (local_id.blank? && local_id.match(/^http/)) then
+          xml.tag!("link",
+                   "rel"  => "alternate",
+                   "href" => local_id)
+        end
+      end
+      xml.tag!("title", obj.what)
+      xml.tag!("author") do
+        xml.tag!("name", obj.who)
+      end
+      xml.tag!("updated", obj.modified)
+      if (!obj.created.blank?) then
+        xml.tag!("published", obj.created)
+      end
+      obj.files.each do |file|
+        xml.tag!("link", 
+                 "href" => url_for(:controller => 'file', 
+                                   :action     => 'display',
+                                   :object     => clean_id(ark),
+                                   :version    => obj.versions.last.identifier,
+                                   :file       => file.identifier),
+                 "rel"  => "http://purl.org/dc/terms/hasPart",
+                 "length" => file.size,
+                 "type"  => file.media_type)
+      end
+    end
+  end
+end
