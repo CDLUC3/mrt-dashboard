92a0d0f7549a50381eb5dc2147a8dcc3a8e7a2ca
Erik Hetzner <erik.hetzner@ucop.edu>
2012-08-14 12:47:03 -0700

    remove cruft

--- app/controllers/help_controller.rb
+++ app/controllers/help_controller.rb
@@ -1,8 +1,3 @@
 class HelpController < ApplicationController
-  #before_filter :require_user
   before_filter :group_optional
-  #before_filter :first_group_if_unset  #wackiness in which tracy included group information in help files breadcrumbs even though possibly not set from choose groups page
-  #before_filter :require_group
-  #layout choose_layout
-
 end
