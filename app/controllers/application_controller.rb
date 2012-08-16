bce86a8200cf48e32577e849ec6694eb50bcc3c4
Erik Hetzner <egh@e6h.org>
2012-08-16 10:43:05 -0700

    Do not randomly catch exceptions.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -167,20 +167,12 @@ class ApplicationController < ActionController::Base
 
   def require_object
     redirect_to(ObjectList.merge({:group => flexi_group_id})) and return false if params[:object].nil?
-    begin
-      @object = UriInfo.new("#{RDF_ARK_URI}#{params[:object]}")
-    rescue Exception => ex
-      redirect_to(ObjectList.merge({:group => flexi_group_id})) and return false
-    end
+    @object = MrtObject.find_by_identifier(params[:object])
   end
 
   def require_mrt_object
     redirect_to(ObjectList.merge({:group => flexi_group_id})) and return false if params[:object].nil?
-    begin
-      @object = MrtObject.find_by_identifier(params[:object])
-    rescue Exception => ex
-      redirect_to(ObjectList.merge({:group => flexi_group_id})) and return false
-    end
+    @object = MrtObject.find_by_identifier(params[:object])
   end
   
   def require_mrt_version
