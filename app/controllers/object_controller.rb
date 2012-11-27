ef731a103a2ed5c3102b7e8b791db08a651093a6
marisa strong <mxs4ever@gmail.com>
2012-11-26 16:28:22 -0800

    merged stage branch into suf branch

--- app/controllers/object_controller.rb
+++ app/controllers/object_controller.rb
@@ -1,13 +1,13 @@
 require 'tempfile'
 
 class ObjectController < ApplicationController
-  before_filter :require_user,            :except => [:jupload_add, :recent, :ingest, :mint]
-  before_filter :require_group,           :except => [:jupload_add, :recent, :ingest, :mint]
-  before_filter :require_write,           :only => [:add, :upload]
-  before_filter :require_session_object,  :only => [:download]
-  before_filter :require_mrt_object,      :only => [:download]
-  before_filter :require_size,            :only => [:download]
-  protect_from_forgery                    :except => [:ingest, :mint]
+  before_filter :require_user,       :except => [:jupload_add, :recent, :ingest, :mint, :update]
+  before_filter :require_group,      :except => [:jupload_add, :recent, :ingest, :mint, :update]
+  before_filter :require_write,      :only => [:add, :upload]
+  before_filter :require_session_object, :only => [:download]
+  before_filter :require_mrt_object, :only => [:download]
+  before_filter :require_size,		 :only => [:download]
+  protect_from_forgery :except => [:ingest, :mint, :update]
 
   def require_session_object
       params[:object] = session[:object] if !session[:object].nil? && params[:object].nil?
@@ -68,6 +68,56 @@ class ObjectController < ApplicationController
         render :status=>response.code, :content_type=>response.headers[:content_type], :text=>response.body
       end
     end
+  end
+
+  def update
+    if !current_user then
+      render :status=>401, :text=>"" and return
+    else
+      if (!params[:file].respond_to? :tempfile) then
+        render(:status=>400, :text=>"Bad file parameter.\n") and return
+      elsif !current_user.groups('write').any? {|g| g.submission_profile == params[:profile]} then
+        render(:status=>404, :text=>"") and return
+      else
+        ingest_args = {
+          'creator'           => params[:creator],
+          'date'              => params[:date],
+          'digestType'        => params[:digestType],
+          'digestValue'       => params[:digestValue],
+          'file'              => params[:file].tempfile,
+          'filename'          => (params[:filename] || params[:file].original_filename),
+          'localIdentifier'   => params[:localIdentifier],
+          'notification'      => params[:notification],
+          'notificationFormat'      => params[:notificationFormat],
+          'primaryIdentifier' => params[:primaryIdentifier],
+          'profile'           => params[:profile],
+          'note'              => params[:note],
+          'responseForm'      => params[:responseForm],
+          'DataCite.resourceType'      => params["DataCite.resourceType"],
+          'DC.contributor'    => params["DC.contributor"],
+          'DC.coverage'       => params["DC.coverage"],
+          'DC.creator'        => params["DC.creator"],
+          'DC.date'           => params["DC.date"],
+          'DC.description'    => params["DC.description"],
+          'DC.format'         => params["DC.format"],
+          'DC.identifier'     => params["DC.identifier"],
+          'DC.language'       => params["DC.language"],
+          'DC.publisher'      => params["DC.publisher"],
+          'DC.relation'       => params["DC.relation"],
+          'DC.rights'         => params["DC.rights"],
+          'DC.source'         => params["DC.source"],
+          'DC.subject'        => params["DC.subject"],
+          'DC.title'          => params["DC.title"],
+          'DC.type'           => params["DC.type"],
+          'submitter'         => "#{current_user.login}/#{current_user.displayname}",
+          'title'             => params[:title],
+          'type'              => params[:type]
+        }.reject{|k, v| v.blank? }
+
+        response = RestClient.post(INGEST_SERVICE_UPDATE, ingest_args, { :multipart => true })
+        render :status=>response.code, :content_type=>response.headers[:content_type], :text=>response.body
+      end
+    end  
   end
   
   def mint
