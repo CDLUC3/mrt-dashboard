02e825bc37e85b7a89fc9488987cbd41494ca6e3
Erik Hetzner <egh@e6h.org>
2012-12-05 16:52:18 -0800

    fix object index

--- app/controllers/object_controller.rb
+++ app/controllers/object_controller.rb
@@ -135,17 +135,12 @@ class ObjectController < ApplicationController
   end
 
   def index
-    begin
-      @object = MrtObject.find_by_identifier(params[:object])
-      @versions = @object.versions
-      #files for current version
-      @files = @object.files.
-        reject {|file| file.identifier.match(/^system\/mrt-/) }.
-        sort_by {|x| File.basename(x.identifier.downcase) }     
-    rescue Exception => ex
-      raise ErrorUnavailable
-    end
-
+    @object = MrtObject.find_by_primary_id(params[:object])
+    @versions = @object.versions
+    #files for current version
+    @files = @object.files.
+      reject {|file| file.identifier.match(/^system\/mrt-/) }.
+      sort_by {|x| File.basename(x.identifier.downcase) }    
   end
 
   def download
