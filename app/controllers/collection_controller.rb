3481416d196992611a775974471dd16b170b0855
Erik Hetzner <egh@e6h.org>
2012-12-05 16:52:05 -0800

    fix collection index

--- app/controllers/collection_controller.rb
+++ app/controllers/collection_controller.rb
@@ -31,14 +31,12 @@ class CollectionController < ApplicationController
   end
 
   def index
-    #redirect objects to the object controller
-    if !params[:object].nil? then
-        redirect_to :controller=>'object', :action=>'index', :group=>params[:group], :object=>params[:object]
-    end
-      
-    @recent_objects = MrtObject.paginate(:collection => no_inject(@group.sparql_id),
-                                         :page       => (params[:page] || 1), 
-                                         :per_page   => 10)
+    @recent_objects = MrtObject.joins(:mrt_collections).
+      where("mrt_collections.ark = ?", @group.ark_id).
+      order('last_add_version desc').
+      includes(:mrt_versions, :mrt_version_metadata).
+      paginate(:page       => (params[:page] || 1), 
+               :per_page   => 10)
   end
 
   def search_results
