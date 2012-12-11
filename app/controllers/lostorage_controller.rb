2f981630051b057dd9f67e1d59b8714438274cf5
marisa strong <mxs4ever@gmail.com>
2012-12-11 09:45:07 -0800

    remove debug stmt

--- app/controllers/lostorage_controller.rb
+++ app/controllers/lostorage_controller.rb
@@ -81,7 +81,6 @@ class LostorageController < ApplicationController
     end
 
   def create_email_msg_body(email)
-    debugger
      to_email = Array(email).concat( APP_CONFIG['lostorage_email_to'])          
      session[:version].nil? ? container_type = "object" : container_type = "version"
      #Create theemail URL to include in the body which includes a random name for stored container
