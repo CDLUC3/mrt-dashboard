b57cc61f7bc810014ae624fc0dd6b736d49153bf
mark reyes <mark.reyes@ucop.edu>
2013-04-10 16:11:58 -0700

    update for VM stage

--- config/environments/stage.rb
+++ config/environments/stage.rb
@@ -8,14 +8,15 @@ MrtDashboard::Application.configure do
   config.serve_static_assets               = true
   config.active_support.deprecation        = :log
 
-  INGEST_SERVICE      = 'http://dp01.cdlib.org:33121/poster/submit/'
-  INGEST_SERVICE_UPDATE   = 'http://dp01.cdlib.org:33121/poster/update/'
+  INGEST_SERVICE      = 'http://uc3-web-stg.cdlib.org:33121/poster/submit/'
+  INGEST_SERVICE_UPDATE   = 'http://uc3-web-stg.cdlib.org:33121/poster/update/'
   MERRITT_SERVER      = 'http://merritt-stage.cdlib.org'
-  MINT_SERVICE        = 'http://dp01.cdlib.org:33121/ingest/request-identifier'
+  MINT_SERVICE        = 'http://uc3-web-stg.cdlib.org:33121/ingest/request-identifier'
   N2T_URI             = "http://n2t-wf.cdlib.org/"
   RDF_ARK_URI         = "http://ark.cdlib.org/"
   RDF_COLLECTION_URI  = "http://uc3.cdlib.org/collection/"
-  CONTAINER_URL       = 'http://merritt-stage.cdlib.org/container/'
+  # CONTAINER_URL       = 'http://merritt-stage.cdlib.org/container/'
+  CONTAINER_URL       = 'http://uc3-mrt-store-stg.cdlib.org:35121/container/'
 
   MAX_ARCHIVE_SIZE    = 4294967295  #maximum size threshhold for download of object/versions without compression
 end
