799e131f01935a2ea482b0e0ca8e94dd8e9cb63a
sfaenza <shirin.faenza@ucop.edu>
2013-08-26 09:40:48 -0700

    merge with default

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,11 +1,12 @@
 MrtDashboard::Application.configure do
   config.action_controller.perform_caching   = false
-  config.action_mailer.raise_delivery_errors = false
-  #config.action_view.debug_rjs               = true
-  config.active_support.deprecation          = :log
   config.autoload_paths                     += %W(#{config.root}/lib)
   config.cache_classes                       = false
   config.consider_all_requests_local         = true
+  config.active_support.deprecation          = :log
+  
+  config.action_mailer.raise_delivery_errors = false
+  #config.action_view.debug_rjs               = true 
   config.whiny_nils                          = true
 
   
