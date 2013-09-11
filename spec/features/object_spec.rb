a7a7382fca5d3dd4e0b2ddd7552b53a04ba5a7e0
sfaenza <shirin.faenza@ucop.edu>
2013-09-11 04:22:04 -0700

    remove factories

--- spec/features/object_spec.rb
+++ spec/features/object_spec.rb
@@ -1,38 +1,21 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-feature 'object' do
+# feature 'object' do
 
-  background do
-    logs_in_with_my_credentials
-  end
+#   background do
+#     logs_in_with_my_credentials
+#   end
 
   
-  scenario "object TEST TO BE COMPLETED" , :js => true do
-    find(:xpath, "//table/tbody/tr[1]/td[1]/a[1]").click # goes to collection landing page
-    expect(page).to have_content('Demo Merritt')
-    find(:xpath, "//table/tbody/tr[1]/td[1]/a[1]").click
-   # FactoryGirl.create(:inv_object).should be_valid
-  end
+#   scenario "object TEST TO BE COMPLETED" , :js => true do
+#     find(:xpath, "//table/tbody/tr[1]/td[1]/a[1]").click # goes to collection landing page
+#     expect(page).to have_content('Demo Merritt')
+#     FactoryGirl.create(:inv_object)
+#     FactoryGirl.create(:inv_version)
+#     find(:xpath, "//table/tbody/tr[1]/td[1]/a[1]").click
+#   end
 
 
-end
+# end
 
 
-# Capybara.default_wait_time = 20
-
-# /object?group=demo_merritt&amp;object=ark%3A%2F90135%2Fq15h7d7h
-
-# first(:link, "Agree").click
-# find(:xpath, "//tr[contains(.,'Foo')]/td/a", :text => 'manage').click
-
-# http://merritt-dev.cdlib.org/m/demo_merritt
-
-# visit "/sessions/new"
-
-# it should be "//a[@href = 'google.com']"
-
-# /m/:group(.:format)                    collection#index
-#/m/:group/:version(.:format)           version#index
- #                   /d/:object(.:format)                   object#download
- #                   /d/:object/:version(.:format)          version#download
- #                   /d/:object/:version/:file(.:format)    file#display {:file=>/.+/i}
