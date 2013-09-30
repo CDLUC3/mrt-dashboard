65b59b45b58e85b448b18f4a6a4bde257f5e49c7
sfaenza <shirin.faenza@ucop.edu>
2013-09-30 14:27:25 -0700

    merge with default

---
+++ lib/institution_ldap.rb
@@ -0,0 +1,19 @@
+module InstitutionLdap
+  class Server
+    include LdapMixin
+
+    def ns_dn(id)
+      "o=#{id},#{@base}"
+    end
+
+    def obj_filter(id)
+      Net::LDAP::Filter.eq("o", id )
+    end
+
+    def find_all
+      return @admin_ldap.search(:base => @base).
+        reject{ |i| i['o'][0] == 'institutions' }.
+        sort_by{ |i| i['o'][0].downcase }
+    end
+  end
+end
