d642a87f6cdcce74c1f0dbb55282ff79c4a63270
Erik Hetzner <egh@e6h.org>
2013-10-30 15:58:22 -0700

    remove comments

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,6 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'capybara/rspec'
 
-#require 'factory_girl_rails'
-#require 'capybara-webkit'
-
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
