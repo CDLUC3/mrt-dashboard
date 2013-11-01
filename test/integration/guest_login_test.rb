b3cf96f216ce56d3e5eccad02d64a40d44078180
Erik Hetzner <erik.hetzner@ucop.edu>
2013-11-01 16:34:22 -0700

    add tests for user login, user login with a single collection

--- test/integration/guest_login_test.rb
+++ test/integration/guest_login_test.rb
@@ -5,4 +5,19 @@ class GuestLoginTest < ActionDispatch::IntegrationTest
     visit login_path
     click_button "Guest"
   end
+  
+  test "login works" do
+    visit(login_path)
+    fill_in("login", :with=>"merritt-test")
+    fill_in("password", :with=>"test")
+    click_button("Login")
+  end
+
+  test "user with single collection is redirected" do
+    visit(login_path)
+    fill_in("login", :with=>"merritt-test")
+    fill_in("password", :with=>"test")
+    click_button("Login")
+    assert_equal(current_path, "/m/opencontext")
+  end
 end
