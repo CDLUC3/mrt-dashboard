9a862ae471adf5e6592ce826ba826df90876725a
Erik Hetzner <egh@e6h.org>
2013-11-01 16:34:51 -0700

    guest_login_test -> login_test

--- test/integration/login_test.rb
+++ test/integration/login_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class GuestLoginTest < ActionDispatch::IntegrationTest
+class LoginTest < ActionDispatch::IntegrationTest
   test "guest login works" do
     visit login_path
     click_button "Guest"
