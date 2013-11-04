08a69d84988e6f8a11453d37eaf01ab7ac4391b2
Erik Hetzner <egh@e6h.org>
2013-11-04 13:04:40 -0800

    extract login/logout in object view test to setup

--- test/integration/object_view_test.rb
+++ test/integration/object_view_test.rb
@@ -3,17 +3,18 @@ require 'test_helper'
 class ObjectViewTest < ActionDispatch::IntegrationTest
   fixtures :inv_objects, :inv_collections_inv_objects, :inv_collections
 
-  test "view object works" do
+  setup do
     visit(logout_path)
     visit(login_path)
+  end
+
+  test "view object works" do
     click_button("Guest")
     click_link("Demo Merritt")
     click_link("ark:/99999/fk40k2sqf")
   end
 
   test "dua works" do
-    visit(logout_path)
-    visit(login_path)
     click_button("Guest")
     click_link("Demo Merritt")
     click_link("ark:/99999/fk40k2sqf")
