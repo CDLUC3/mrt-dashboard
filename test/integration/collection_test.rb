5c4da4eb799eb19386015e3308f56b64a594fa65
Erik Hetzner <erik.hetzner@ucop.edu>
2013-11-04 17:02:25 -0800

    add test for visiting collection via /m/ark without login

---
+++ test/integration/collection_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class CollectionTest < ActionDispatch::IntegrationTest
+  fixtures :inv_objects, :inv_collections_inv_objects, :inv_collections
+
+  test "/m/ access to collection works without login" do
+    visit(logout_path)
+    visit("/m/ark%3A%2F99999%2Ffk4q24532")
+    assert_equal(200, page.status_code)
+  end
+end
