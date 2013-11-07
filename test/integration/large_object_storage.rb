aad9e0cbc7ae54427b8246d1794afba4a2e5a02c
Erik Hetzner <egh@e6h.org>
2013-11-06 16:17:55 -0800

    add test for canceling large object download

--- test/integration/large_object_storage.rb
+++ test/integration/large_object_storage.rb
@@ -8,6 +8,14 @@ class LargeObjectStorageTest < ActionDispatch::IntegrationTest
     visit(login_path)
   end
 
+  def fill_out_dua
+    fill_in('Name', :with => 'Jane Doe')
+    fill_in('Affiliation', :with => 'Doe International')
+    fill_in('Email', :with => 'doe@mailinator.com')
+    check("accept")
+    click_button("Accept")
+  end
+  
   test "large object download works" do 
     click_button("Guest")
     click_link("Demo Merritt")
@@ -15,14 +23,20 @@ class LargeObjectStorageTest < ActionDispatch::IntegrationTest
     click_button("Download object")
 
     # should be sent to dua page
-    fill_in('Name', :with => 'Jane Doe')
-    fill_in('Affiliation', :with => 'Doe International')
-    fill_in('Email', :with => 'doe@mailinator.com')
-    check("accept")
-    click_button("Accept")
+    fill_out_dua()
 
     # now at large object page
     fill_in("Email", :with => "doe@mailinator")
     click_button("Submit")
+    assert_equal(current_path, "/m/ark%3A%2F99999%2Ffk41z6855")
+    
+    visit("/d/ark%3A%2F99999%2Ffk41z6855")
+    # should be sent to dua page again
+    # but we aren't, because of a bug ?
+    # fill_out_dua()
+
+    # now at large object page, see if we can cancel
+    click_button("Cancel")
+    assert_equal(current_path, "/m/ark%3A%2F99999%2Ffk41z6855")
   end
 end
