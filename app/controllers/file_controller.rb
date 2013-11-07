bcfd4faf3e4944203e0be0eb690f16e8a33376bc
Erik Hetzner <erik.hetzner@ucop.edu>
2013-11-07 14:25:13 -0800

    remove unneeded group param

--- app/controllers/file_controller.rb
+++ app/controllers/file_controller.rb
@@ -33,7 +33,7 @@ class FileController < ApplicationController
         # if DUA was not accepted, redirect to object landing page 
         if session[:collection_acceptance][file.inv_version.inv_object.group.id].eql?("not accepted") then
           session[:collection_acceptance][file.inv_version.inv_object.group.id] = false  # reinitialize to false so user can again be given option to accept DUA 
-          redirect_to  :controller => 'object', :action => 'index', :group => flexi_group_id,  :object =>params[:object] and return false         
+          redirect_to  :controller => 'object', :action => 'index', :object =>params[:object] and return false         
           # if DUA for this collection has not yet been displayed to user, perform logic to retrieve DUA.
           # if persistance is at the session level and user already saw DUA, this section will be skipped
         elsif !session[:collection_acceptance][file.inv_version.inv_object.group.id]
