5e85395b8450e32e84746411a072b04115790eb7
Erik Hetzner <egh@e6h.org>
2013-11-08 13:37:20 -0800

    use ==, not eql? to compare

--- app/controllers/dua_controller.rb
+++ app/controllers/dua_controller.rb
@@ -6,7 +6,7 @@ class DuaController < ApplicationController
 
   def index 
     dua_hash ||= Dua.parse_file(fetch_to_tempfile(session[:dua_file_uri]))
-    if params['commit'].eql?("Accept") then  
+    if params['commit'] == "Accept" then
       flash[:message] = 'You must check that you accept the terms.' and return if params[:accept].blank?
       if params[:name].blank? || params[:affiliation].blank? || params[:user_agent_email].blank? then
         flash[:message] = 'Please enter the required fields' and return
@@ -27,14 +27,14 @@ class DuaController < ApplicationController
                           :terms       => dua_hash["Terms"]).deliver
       #user accepted DUA, go ahead and process file/object/version download
       # set the persistence flag for session level so DUA doesn't get displayed again for this session
-      if dua_hash["Persistence"].eql?("session") then
+      if dua_hash["Persistence"] == "session" then
         session[:collection_acceptance][group.id] = true
       end
       # return to where user came from 
       session[:perform_download] = true
       # TODO too many slashes here if some params are empty
       redirect_to "/d/#{urlencode_mod(params[:object])}/#{params[:version]}/#{params[:file]}"
-    elsif params[:commit].eql?("Do Not Accept") then
+    elsif (params[:commit] == "Do Not Accept") then
       session[:collection_acceptance][group.id] = "not accepted"
       # TODO too many slashes here if some params are empty
       redirect_to "/d/#{urlencode_mod(params[:object])}/#{params[:version]}/#{params[:file]}"
