f42df71ca27b6b0a09ee122d0c420c794d77a3d1
Erik Hetzner <erik.hetzner@ucop.edu>
2013-11-12 10:57:45 -0800

    move dua stuff to filter

--- app/controllers/file_controller.rb
+++ app/controllers/file_controller.rb
@@ -1,6 +1,7 @@
 class FileController < ApplicationController
   before_filter :require_user
   before_filter :load_file
+
   before_filter do
     if (!has_object_permission?(@file.inv_version.inv_object, 'download')) then
       flash[:error] = "You do not have download permissions."
@@ -10,6 +11,14 @@ class FileController < ApplicationController
     end
   end
 
+  before_filter(:only => [:download]) do
+    check_dua(@file.inv_version.inv_object.group.id, 
+              @file, 
+              { :object  => @file.inv_version.inv_object,
+                :version => @file.inv_version,
+                :file    => @file})
+  end
+  
   def load_file
     filename = params_u(:file)
 
@@ -28,30 +37,6 @@ class FileController < ApplicationController
   end
 
   def download
-    # bypass DUA processing for python scripts - indicated by special param
-    if params[:blue].nil? then
-      session[:collection_acceptance] ||= Hash.new(false)
-      #check if user already saw DUA and accepted- if so, skip all this & download the file
-      if !session[:perform_download]  
-        # if DUA was not accepted, redirect to object landing page 
-        if !session[:collection_acceptance][@file.inv_version.inv_object.group.id]
-          # perform DUA logic to retrieve DUA
-          #construct the dua_file_uri based off the file_uri, the object's parent collection, version 0, and  DUA filename
-          dua_file_uri = construct_dua_uri(@file.dua_rx, @file.bytestream_uri)
-          if process_dua_request(dua_file_uri) then
-          # if the DUA for this collection exists, display DUA to user for acceptance before displaying file
-            session[:dua_file_uri] = dua_file_uri
-            redirect_to(:controller => "dua", 
-                        :action => "index",
-                        :object => @file.inv_version.inv_object,
-                        :version => @file.inv_version,
-                        :file => @file) and return
-          end
-        end
-      end
-    end
-    
-    # the user has accepted the DUA for this collection or there is no DUA to process 
     stream_response(@file.bytestream_uri, 
                     "inline",
                     File.basename(@file.pathname), 
--- app/controllers/version_controller.rb
+++ app/controllers/version_controller.rb
@@ -10,6 +10,13 @@ class VersionController < ApplicationController
     end
   end
 
+  before_filter(:only => [:download]) do
+    check_dua(@version.inv_object.group.id, 
+              @version,
+              { :object  => @version.inv_object,
+                :version => @version})
+  end
+
   def load_version
     if (params[:version].to_i == 0) then
       latest_version = InvObject.find_by_ark(params_u(:object)).current_version.number
@@ -28,24 +35,6 @@ class VersionController < ApplicationController
   end
 
   def download
-    # bypass DUA processing for python scripts - indicated by special param
-    if params[:blue].nil? then
-      session[:collection_acceptance] ||= Hash.new(false)
-      #check if user already saw DUA and accepted- if so, skip all this & download the file
-      if !session[:perform_download]   
-        if !session[:collection_acceptance][@version.inv_object.group.id]
-          # perform DUA logic to retrieve DUA
-          #construct the dua_file_uri based off the version uri, the object's parent collection, version 0, and  DUA filename
-          dua_file_uri = construct_dua_uri(@version.dua_rx, @version.bytestream_uri)
-          if process_dua_request(dua_file_uri) then
-            # if the DUA for this collection exists, display DUA to user for acceptance before displaying file
-            session[:dua_file_uri] = dua_file_uri
-            redirect_to(:controller => "dua", :action => "index", :object => @version.inv_object, :version => @version) and return false 
-          end
-        end
-      end
-    end
-
     # if size is > 4GB, redirect to have user enter email for asynch compression (skipping streaming)
     if exceeds_size(@version.inv_object) then
       redirect_to(:controller => "lostorage", :action => "index", :object => @version.inv_object, :version => @version) and return
