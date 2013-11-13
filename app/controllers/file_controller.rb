eaa9433c6f73082c790e00320a126bfc72a4f788
Erik Hetzner <egh@e6h.org>
2013-11-13 11:49:17 -0800

    fix redirect when user does not have file download permissions

--- app/controllers/file_controller.rb
+++ app/controllers/file_controller.rb
@@ -5,7 +5,8 @@ class FileController < ApplicationController
   before_filter do
     if (!has_object_permission?(@file.inv_version.inv_object, 'download')) then
       flash[:error] = "You do not have download permissions."
-      redirect_to(:action  => :index, 
+      redirect_to(:controller => :version,
+                  :action  => :index, 
                   :object  => @file.inv_version.inv_object, 
                   :version => @file.inv_version) and return false
     end
