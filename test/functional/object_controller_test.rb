0a17064b0ecc65568f66c8b59ba9e354a9c38ce5
Erik Hetzner <erik.hetzner@ucop.edu>
2013-11-14 13:43:20 -0800

    prevent viewing objects that users do not have permissions to view

--- test/functional/object_controller_test.rb
+++ test/functional/object_controller_test.rb
@@ -5,4 +5,9 @@ class ObjectControllerTest < ActionController::TestCase
     get(:index, {:object => "ark:/99999/fk40k2sqf"}, {:uid => "anonymous"})
     assert_response(:success)
   end
+
+  test "prevent view object not allowed" do
+    get(:index, {:object => "ark:/99999/fk40k2sqf"}, {:uid => "merritt-test"})
+    assert_redirected_to(:controller=>:home, :action=>:index)
+  end
 end
