76d9e5c5fbab9d96b18f1dd1cd8fa5fe0a3fb41d
Erik Hetzner <egh@e6h.org>
2013-11-14 11:09:03 -0800

    add mk_httpclient method

--- app/controllers/object_controller.rb
+++ app/controllers/object_controller.rb
@@ -229,4 +229,14 @@ class ObjectController < ApplicationController
       format.atom
     end
   end
+  
+  private
+  def mk_httpclient
+    client = HTTPClient.new
+    client.receive_timeout = 7200
+    client.send_timeout = 3600
+    client.connect_timeout = 7200
+    client.keep_alive_timeout = 3600
+    client
+  end
 end
