406ade33ec1a7826fe31044a96cedea04b33b9c5
Erik Hetzner <erik.hetzner@ucop.edu>
2013-11-13 16:45:08 -0800

    check where we are redirected to

--- test/functional/version_controller_test.rb
+++ test/functional/version_controller_test.rb
@@ -3,12 +3,12 @@ require 'test_helper'
 class VersionControllerTest < ActionController::TestCase
   test "version 0 redirects to latest" do
     get(:index, {:object => "ark:/99999/fk40k2sqf", :version=>0}, {:uid => "anonymous"})
-    assert_response(302)
+    assert_redirected_to(:action => :index, :object => "ark:/99999/fk40k2sqf", :version=>1)
   end
 
   test "version 0 download redirects to latest" do
     get(:download, {:object => "ark:/99999/fk40k2sqf", :version=>0}, {:uid => "anonymous"})
-    assert_response(302)
+    assert_redirected_to(:action => :download, :object => "ark:/99999/fk40k2sqf", :version=>1)
   end
 
   test "index" do
