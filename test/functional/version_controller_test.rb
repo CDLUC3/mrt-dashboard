9467b198139b6adcae3e5d11718ba391c36c247a
Erik Hetzner <egh@e6h.org>
2013-11-15 14:42:30 -0800

    add some new fixtures, test_inv_version

--- test/functional/version_controller_test.rb
+++ test/functional/version_controller_test.rb
@@ -27,4 +27,28 @@ class VersionControllerTest < ActionController::TestCase
     assert_select(HTML::Selector.new("div.key:content(object primary identifier:) + div.value"), 
                   :text=>"ark:/99999/fk40k2sqf")
   end
+
+  test "different metadata for different versions" do
+    get(:index, {:object => "ark:/99999/diffmeta", :version=>1}, {:uid => "anonymous"})
+    assert_response(:success)
+    assert_select(HTML::Selector.new("div.key:content(version number:) + div.value"), 
+                  :text=>"1")
+    assert_select(HTML::Selector.new("div.key:content(title:) + div.value"), 
+                  :text=>"Title 1&nbsp;")
+    assert_select(HTML::Selector.new("div.key:content(creator:) + div.value"), 
+                  :text=>"Author 1&nbsp;")
+    assert_select(HTML::Selector.new("div.key:content(date:) + div.value"), 
+                  :text=>"2002&nbsp;")
+
+    get(:index, {:object => "ark:/99999/diffmeta", :version=>2}, {:uid => "anonymous"})
+    assert_response(:success)
+    assert_select(HTML::Selector.new("div.key:content(version number:) + div.value"), 
+                  :text=>"2")
+    assert_select(HTML::Selector.new("div.key:content(title:) + div.value"), 
+                  :text=>"Title 2&nbsp;")
+    assert_select(HTML::Selector.new("div.key:content(creator:) + div.value"), 
+                  :text=>"Author 2&nbsp;")
+    assert_select(HTML::Selector.new("div.key:content(date:) + div.value"), 
+                  :text=>"2012&nbsp;")
+  end
 end
