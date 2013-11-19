63d75da8bfcc3a481deabf48f1ff39cc88f00a44
Erik Hetzner <egh@e6h.org>
2013-11-19 13:18:54 -0800

    merge from default

--- app/models/inv_file.rb
+++ app/models/inv_file.rb
@@ -1,13 +1,15 @@
 class InvFile < ActiveRecord::Base
   belongs_to :inv_version, :inverse_of => :inv_files
   belongs_to :inv_object
+  scope :system_files, lambda { where("pathname LIKE 'system/%'") }
+  scope :producer_files, lambda { where("pathname LIKE 'producer/%'") }
 
   include Encoder
 
   def to_param
     urlencode(self.pathname)
   end
-  
+
   def bytestream_uri
     URI.parse("#{APP_CONFIG['uri_1']}#{self.inv_version.inv_object.node_number}/#{self.inv_version.inv_object.to_param}/#{self.inv_version.to_param}/#{self.to_param}")
   end
