398109e932e759767435e2f582191be19a01e0c2
Mark Reyes <mark.reyes@ucop.edu>
2013-12-06 12:10:59 -0800

    merge atom change to stage

---
+++ app/controllers/dua_controller.rb
@@ -0,0 +1,35 @@
+class DuaController < ApplicationController
+  before_filter :require_user
+  
+  def index 
+    object = InvObject.where("inv_objects.ark = ?", params_u(:object)).first
+    dua_hash = with_fetched_tempfile(object.dua_uri) { |f| Dua.parse_file(f) }
+    if params['commit'] == "Accept" then
+      flash[:message] = 'You must check that you accept the terms.' and return if params[:accept].blank?
+      if params[:name].blank? || params[:affiliation].blank? || params[:user_agent_email].blank? then
+        flash[:message] = 'Please enter the required fields' and return
+      end
+      if !params[:user_agent_email].match(/^.+@.+$/)
+        flash[:message] = 'You must fill in a valid return email address.' and return
+      end   
+      
+      group = object.group
+      DuaMailer.dua_email(:to          => params[:user_agent_email],
+                          :cc          => APP_CONFIG['dua_email_to'] + [dua_hash["Notification"] || ''],
+                          :reply_to    => dua_hash["Notification"],
+                          :title       => dua_hash["Title"],
+                          :name        => params[:name],
+                          :affiliation => params[:affiliation],
+                          :object      => params_u(:object),
+                          :collection  => group.description,
+                          :terms       => dua_hash["Terms"]).deliver
+      #user accepted DUA, go ahead and process file/object/version download
+      session[:collection_acceptance][group.id] = (dua_hash["Persistence"] || "single")
+      redirect_to mk_merritt_url("d", params[:object], params[:version], params[:file])
+    elsif (params[:commit] == "Do Not Accept") then
+      redirect_to mk_merritt_url("m", params[:object], params[:version])
+    else
+      @title, @terms = dua_hash['Title'], dua_hash['Terms']
+    end
+  end
+end
---
+++ app/models/inv_object.rb
@@ -0,0 +1,59 @@
+class InvObject < ActiveRecord::Base
+  has_many :inv_versions, :inverse_of => :inv_object
+  has_many :inv_files, :through => :inv_versions
+  
+  has_many :inv_dublinkernels
+
+  has_many :inv_collections_inv_objects
+  has_many :inv_collections, :through => :inv_collections_inv_objects
+
+  has_many :inv_nodes_inv_objects
+  has_many :inv_nodes, :through => :inv_nodes_inv_objects
+
+  # hack to fix erc_ tables taking forever to load
+  scope :quickloadhack, lambda { 
+    select(["inv_objects.id", "inv_objects.version_number", "inv_objects.inv_owner_id", "inv_objects.object_type", "inv_objects.role", "inv_objects.aggregate_role", "inv_objects.ark", "inv_objects.created", "inv_objects.modified", "inv_objects.id", "inv_objects.ark", "inv_objects.created", "inv_objects.modified"])
+  }
+
+  include Encoder
+
+  def to_param
+    urlencode(self.ark)
+  end
+
+  def bytestream_uri
+    URI.parse("#{APP_CONFIG['uri_1']}#{self.node_number}/#{self.to_param}")
+  end
+
+  def dua_uri
+    URI.parse("#{APP_CONFIG['uri_1']}#{self.node_number}/#{self.inv_collection.to_param}/0/#{urlencode(APP_CONFIG['mrt_dua_file'])}")
+  end
+
+  def node_number
+    self.inv_nodes.where("inv_nodes_inv_objects.role" => "primary").select("inv_nodes.number").map(&:number).first
+  end
+
+  def size
+    self.inv_files.sum("billable_size")
+  end
+ 
+  def total_actual_size
+    self.inv_files.sum("full_size")
+  end
+
+  def current_version
+    @current_version ||= self.inv_versions.order("number desc").first
+  end
+  
+  def inv_collection
+    @inv_collection ||= self.inv_collections.first
+  end
+
+  def group
+    self.inv_collection.group
+  end
+  
+  def permalink
+    "#{APP_CONFIG['n2t_uri']}#{self.ark.to_s}"
+  end
+end
