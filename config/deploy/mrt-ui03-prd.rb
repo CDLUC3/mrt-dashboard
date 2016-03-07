b86a4f207c54ef488193b4fa7618735e7873fbbc
mark reyes <mark.reyes@ucop.edu>
2016-03-07 10:41:57 -0800

    correct RAILS_ENV value

--- config/deploy/mrt-ui03-prd.rb
+++ config/deploy/mrt-ui03-prd.rb
@@ -15,7 +15,7 @@
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-set :rails_env, "prod"
+set :rails_env, "production"
 
 puts "Production branch"
 set :branch, 'prod'
