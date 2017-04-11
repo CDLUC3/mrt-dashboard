799801884e4d9ddf303502c0ec3d1578b11b385e
Mark Reyes <mark.reyes@ucop.edu>
2017-04-11 16:36:58 -0700

    Switch deployment files to Puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,38 +30,38 @@ set :keep_releases, 5
 
 namespace :deploy do
 
-  desc 'Stop Unicorn'
+  desc 'Stop Puma'
   task :stop do
     on roles(:app) do
-      if test("[ -f #{fetch(:unicorn_pid)} ]")
-        execute "cd #{deploy_to}/current; kill -15 `cat #{fetch(:unicorn_pid)}`"
+      if test("[ -f #{fetch(:puma_pid)} ]")
+        execute "cd #{deploy_to}/current; kill -15 `cat #{fetch(:puma_pid)}`"
       end
     end
   end
 
-  desc 'Start Unicorn'
+  desc 'Start Puma'
   task :start do
     on roles(:app) do
       within current_path do
         with rails_env: fetch(:rails_env) do
-          execute "cd #{deploy_to}/current; bundle exec unicorn --config-file config/unicorn/#{fetch(:rails_env)}.rb --port #{fetch(:unicorn_port)} --env #{fetch(:rails_env)} --daemonize"
+          execute "cd #{deploy_to}/current; bundle exec puma -C config/puma/#{fetch(:rails_env)}.rb -e #{fetch(:rails_env)}"
         end
       end
     end
   end
   before "deploy:start", "bundle:install"
 
-  desc 'Status Unicorn'
+  desc 'Status Puma'
   task :status do
     on roles(:app) do
-      if test("[ -f #{fetch(:unicorn_pid)} ]")
+      if test("[ -f #{fetch(:puma_pid)} ]")
          # check pid
-         execute "cd #{deploy_to}/current; cat #{fetch(:unicorn_pid)} | xargs ps -lp"
+         execute "cd #{deploy_to}/current; cat #{fetch(:puma_pid)} | xargs ps -lp"
       end
     end
   end
 
-  desc 'Restart Unicorn'
+  desc 'Restart Puma'
   task :restart do
     on roles(:app), wait: 5 do
        # do not implement, use stop/start instead
