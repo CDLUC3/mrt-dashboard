311092697f405ae098280cd936aab9fa6d162ad0
Mark Reyes <mark.reyes@ucop.edu>
2017-05-04 10:43:13 -0700

    merge with stage

--- config/environments/stage.rb
+++ config/environments/stage.rb
@@ -5,7 +5,8 @@ MrtDashboard::Application.configure do
   config.cache_classes                     = true
   config.consider_all_requests_local       = false
   config.i18n.fallbacks                    = true
-  config.serve_static_assets               = true
+  config.serve_static_files		   = true
+
   config.active_support.deprecation        = :log
 
   config.eager_load                          = false
