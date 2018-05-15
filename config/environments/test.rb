4a7abeda492d7d29166bca7aa3f706c51cc59fe9
David Moles <david.moles@ucop.edu>
2018-05-15 13:56:43 -0700

    Remove obsolete config.active_record.mass_assignment_sanitizer= settings

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,9 +30,6 @@ MrtDashboard::Application.configure do
   # like if you have constraints or database-specific column types
   # config.active_record.schema_format = :sql
 
-  # Raise exception on mass assignment protection for Active Record models
-  config.active_record.mass_assignment_sanitizer = :strict
-
   config.active_support.deprecation = :stderr
 
   config.active_record.schema_format = :sql
