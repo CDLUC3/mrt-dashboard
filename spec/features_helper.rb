cc83dcfb0e4acccb37fc40451d5d893fbb7679ec
David Moles <david.moles@ucop.edu>
2018-05-21 16:06:29 -0700

    Unpin Chromedriver version

--- spec/features_helper.rb
+++ spec/features_helper.rb
@@ -6,10 +6,6 @@ require 'capybara/rspec'
 # ------------------------------------------------------------
 # Capybara etc.
 
-# Ideally we'd set a minimum Chromedriver version, but that's not an
-# option; see https://github.com/flavorjones/chromedriver-helper
-Chromedriver.set_version('2.37')
-
 Capybara.register_driver(:selenium) do |app|
   Capybara::Selenium::Driver.new(
     app,
