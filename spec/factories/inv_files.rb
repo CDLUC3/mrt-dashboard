de31f5d41870bcc9e56408adbd9dc8685aae4ae7
David Moles <david.moles@ucop.edu>
2018-05-23 15:30:18 -0700

    Check that files are listed

--- spec/factories/inv_files.rb
+++ spec/factories/inv_files.rb
@@ -1,4 +1,14 @@
 FactoryBot.define do
-  factory :inv_files do
+  factory :inv_file do
+
+    billable_size 0
+    created { Time.now }
+
+    # noinspection RubyArgCount
+    after(:build) do |file, evaluator|
+      source = evaluator.pathname.start_with?('system') ? 'system' : 'producer'
+      file.source ||= source
+      file.role ||= source == 'producer' ? 'metadata' : 'data'
+    end
   end
 end
--- spec/features/object_spec.rb
+++ spec/features/object_spec.rb
@@ -4,6 +4,8 @@ describe 'objects' do
   attr_reader :user_id
   attr_reader :password
   attr_reader :obj
+  attr_reader :producer_files
+  attr_reader :system_files
 
   before(:each) do
     @password = 'correcthorsebatterystaple'
@@ -16,6 +18,32 @@ describe 'objects' do
     @obj = create(:inv_object, erc_who: 'Doe, Jane', erc_what: 'Object 1', erc_when: '2018-01-01')
     inv_collection_1.inv_objects << obj
 
+    @producer_files = Array.new(3) do |i|
+      size = 1024 * (2 ** i)
+      create(
+        :inv_file,
+        inv_object: obj,
+        inv_version: obj.current_version,
+        pathname: "producer/file-#{i}.bin",
+        full_size: size,
+        billable_size: size,
+        mime_type: 'application/octet-stream'
+      )
+    end
+
+    @system_files = Array.new(3) do |i|
+      size = 1024 * (2 ** i)
+      create(
+        :inv_file,
+        inv_object: obj,
+        inv_version: obj.current_version,
+        pathname: "system/file-#{i}.xml",
+        full_size: size,
+        billable_size: size,
+        mime_type: 'text/xml'
+      )
+    end
+
     log_in_with(user_id, password)
     click_link(obj.ark)
   end
@@ -24,6 +52,13 @@ describe 'objects' do
     expect(page).to have_content(obj.erc_who)
     expect(page).to have_content(obj.erc_what)
     expect(page).to have_content(obj.erc_when)
+  end
+
+  it 'should display the producer files' do
+    producer_files.each do |f|
+      basename = f.pathname.sub(/^producer\//, '')
+      expect(page).to have_content(basename)
+    end
   end
 
   it 'should display a link to the version' do
