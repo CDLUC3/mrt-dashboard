2613c31fbf518b5a3e0e0a80ad82d38e1b888468
David Moles <david.moles@ucop.edu>
2018-06-08 11:04:02 -0700

    Add spec for :number_to_storage_size

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -40,4 +40,45 @@ describe ApplicationController do
       expect(controller.in_embargo?(obj)).to eq(false)
     end
   end
+
+  describe ':render_unavailable' do
+    it 'returns a 500 error' do
+      get(:render_unavailable)
+      expect(response.status).to eq(500)
+    end
+  end
+
+  describe ':number_to_storage_size' do
+    it 'defaults to one digit after the decimal' do
+      {
+        0 => '0 B',
+        1 => '1 Byte',
+        123 => '123 B',
+        1234 => '1.2 KB',
+        12345 => '12.3 KB',
+        1234567 => '1.2 MB',
+        1234567890 => '1.2 GB',
+        1234567890123 => '1.2 TB'
+      }.each do |i, expected|
+        actual = controller.send(:number_to_storage_size, i)
+        expect(actual).to eq(expected)
+      end
+    end
+
+    it 'supports multiple digits after the decimal' do
+      {
+        0 => '0 B',
+        1 => '1 Byte',
+        123 => '123 B',
+        1234 => '1.23 KB',
+        12345 => '12.35 KB', # 12.345 rounds up to 12.35
+        1234567 => '1.23 MB',
+        1234567890 => '1.23 GB',
+        1234567890123 => '1.23 TB'
+      }.each do |i, expected|
+        actual = controller.send(:number_to_storage_size, i, 2)
+        expect(actual).to eq(expected)
+      end
+    end
+  end
 end
