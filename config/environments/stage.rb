c892af7f5611c7b62970ee1940a5830405cd169d
DPR2 Role Account <dpr2@uc3-mrtui01-stg.cdlib.org>
2018-07-09 10:38:02 -0700

    Re-enable asset pipeline.  The issue we had with the pipline was in fact an Apache connection issue upstream

--- config/environments/stage.rb
+++ config/environments/stage.rb
@@ -6,11 +6,29 @@ MrtDashboard::Application.configure do
   config.consider_all_requests_local       = false
   config.i18n.fallbacks                    = true
   config.serve_static_files		   = true
-  config.assets.enabled 		   = false
+
+  # To turn off pipeline, set to false
+  config.assets.enabled  = true
+  
+  # Compress JavaScripts and CSS.
+  config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :yui
+
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = false
+
+  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
+  # yet still be able to expire them through the digest params.
+  config.assets.digest = true
+
+  # Eager load code on boot. This eager loads most of Rails and
+  # your application in memory, allowing both threaded web servers
+  # and those relying on copy on write to perform better.
+  # Rake tasks automatically ignore this option for performance.
+  config.eager_load                         = true
 
   config.active_support.deprecation        = :log
 
-  config.eager_loa                         = false
 end
 
 require 'exception_notifier'
