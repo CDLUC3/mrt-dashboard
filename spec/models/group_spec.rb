f5271efd6f229ba574480cda1a4fa95477b6a1b3
David Moles <david.moles@ucop.edu>
2018-07-10 13:02:32 -0700

    Add unit tests for count methods

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -6,10 +6,10 @@ describe Group do
   attr_reader :group_ldap
 
   before(:each) do
-    @collection = create(:inv_collection, name: 'Collection 1', mnemonic: 'collection_1')
+    @collection   = create(:inv_collection, name: 'Collection 1', mnemonic: 'collection_1')
     collection_id = mock_ldap_for_collection(collection)
-    @group_ldap = Group::LDAP.fetch(collection_id)
-    @group = Group.make_from_ldap(group_ldap)
+    @group_ldap   = Group::LDAP.fetch(collection_id)
+    @group        = Group.make_from_ldap(group_ldap)
   end
 
   describe ':find_all' do
@@ -33,6 +33,63 @@ describe Group do
     it 'wraps the ARK' do
       expected_url = "http://ark.cdlib.org/#{collection.ark}"
       expect(group.sparql_id).to eq(expected_url)
+    end
+  end
+
+  describe ':object_count' do
+    attr_reader :objects
+
+    before(:each) do
+      @objects = Array.new(3) { |i| create(:inv_object, erc_who: 'Doe, Jane', erc_what: "Object #{i}", erc_when: "2018-01-0#{i}") }
+      collection.inv_objects << objects
+    end
+
+    it 'counts objects' do
+      expect(group.object_count).to eq(objects.count)
+    end
+  end
+
+  describe ':version_count' do
+    attr_reader :objects
+
+    before(:each) do
+      @objects = Array.new(3) { |i| create(:inv_object, erc_who: 'Doe, Jane', erc_what: "Object #{i}", erc_when: "2018-01-0#{i}") }
+      collection.inv_objects << objects
+      objects.each do |obj|
+        create(:inv_version, inv_object: obj, number: 2)
+        obj.version_number = 2
+        obj.save!
+      end
+    end
+
+    it 'counts versions' do
+      expect(group.version_count).to eq(2 * objects.count)
+    end
+  end
+
+  describe ':file_count' do
+    attr_reader :objects
+
+    before(:each) do
+      @objects = Array.new(3) { |i| create(:inv_object, erc_who: 'Doe, Jane', erc_what: "Object #{i}", erc_when: "2018-01-0#{i}") }
+      collection.inv_objects << objects
+      objects.each_with_index do |obj, i|
+        v2 = create(:inv_version, inv_object: obj, number: 2)
+        create(
+          :inv_file,
+          inv_object:  obj,
+          inv_version: v2,
+          pathname:    "producer/file-#{i}.bin",
+          source:      'producer',
+          role:        'data'
+        )
+        obj.version_number = 2
+        obj.save!
+      end
+    end
+
+    it 'counts files' do
+      expect(group.file_count).to eq(objects.count)
     end
   end
 end
