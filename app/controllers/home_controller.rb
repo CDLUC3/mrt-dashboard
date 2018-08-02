db68d1b42245af6a4ee0c62763e0885ad2138719
David Moles <david.moles@ucop.edu>
2018-08-02 15:56:47 -0700

    New action for testing new home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
 class HomeController < ApplicationController
   before_filter :require_user, only: :choose_collection
 
+  def index_new
+    respond_to do |format|
+      format.html { render :layout => false }
+    end
+  end
+
   def choose_collection
     return unless available_groups.length == 1
     redirect_to(controller: :collection,
