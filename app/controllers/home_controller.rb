495f28184472f1afbb09948ad76ad32816281ec1
David Moles <david.moles@ucop.edu>
2018-08-08 11:43:50 -0700

    Integrate new styles into layout, sort of

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,6 @@
 class HomeController < ApplicationController
   before_filter :require_user, only: :choose_collection
 
-  def index
-    respond_to do |format|
-      # TODO: remove this once we've converted the new index page into a global layout
-      format.html { render layout: false }
-    end
-  end
-
   def choose_collection
     return unless available_groups.length == 1
     redirect_to(controller: :collection,
