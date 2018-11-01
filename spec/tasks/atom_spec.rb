60eba6aa93b83f9b20a91a2880d36abe6c40f724
David Moles <david.moles@ucop.edu>
2018-11-01 12:34:32 -0700

    Add detailed ingest specs

--- spec/tasks/atom_spec.rb
+++ spec/tasks/atom_spec.rb
@@ -3,10 +3,6 @@ require 'support/tasks'
 require 'webmock/rspec'
 require 'fileutils'
 
-def invoke_update!
-  invoke_task('atom:update', starting_point, submitter, profile, collection, feeddatefile)
-end
-
 describe 'atom', type: :task do
 
   before(:each) do
@@ -46,7 +42,18 @@ describe 'atom', type: :task do
 
     def write_feeddate(date)
       FileUtils.mkdir_p(File.dirname(feeddatefile))
-      open(feeddatefile, 'w') { |f| f.puts(date.utc.iso8601)}
+      open(feeddatefile, 'w') { |f| f.puts(date.utc.iso8601) }
+    end
+
+    def invoke_update!
+      invoke_task('atom:update', starting_point, submitter, profile, collection, feeddatefile)
+    end
+
+    def add_file
+      tmpfile = Tempfile.new
+      @tempfiles << tmpfile
+      File.open(tmpfile, 'w+') { |f| yield f }
+      ["http://ingest.example.edu/#{File.basename(tmpfile)}", tmpfile]
     end
 
     before(:each) do
@@ -59,13 +66,16 @@ describe 'atom', type: :task do
 
       @client = instance_double(Mrt::Ingest::Client)
       allow(Mrt::Ingest::Client).to receive(:new).with(APP_CONFIG['ingest_service']).and_return(client)
+      allow(client).to receive(:ingest)
 
       @server = instance_double(Mrt::Ingest::OneTimeServer)
       allow(Mrt::Ingest::OneTimeServer).to receive(:new).and_return(server)
       allow(server).to receive(:start_server)
       allow(server).to receive(:join_server)
+      allow(server).to(receive(:add_file).with(no_args)) { |&block| add_file(&block) }
 
       @starting_point = 'https://s3.example.com/static.ucldc.example.edu/merritt/ucldc_collection_26144.atom'
+      @submitter = 'Atom processor/Example U Digital Special Collections'
       @profile = 'example_ingest_profile'
       @collection = 'FK5551212'
       @collection_ark = "ark:/99999/#{collection}"
@@ -75,11 +85,14 @@ describe 'atom', type: :task do
       @pause_file = "#{atom_dir}/PAUSE_ATOM_#{profile}"
 
       stub_request(:get, starting_point).to_return(status: 200, body: feed_xml_str, headers: {})
+
+      @tempfiles = []
     end
 
     after(:each) do
       ENV['HOME'] = original_home
       FileUtils.remove_entry_secure(@tmp_home)
+      @tempfiles.each { |f| FileUtils.remove_entry_secure(f) }
     end
 
     it 'starts the one-time file server' do
@@ -91,7 +104,7 @@ describe 'atom', type: :task do
       FileUtils.mkdir_p(atom_dir)
       FileUtils.touch(pause_file)
 
-      # hack to "expect().to receive" global sleep call
+      # HACK: to "expect().to receive" global sleep call
       @sleep_count = 0
       allow_any_instance_of(Object).to receive(:sleep).with(300) do
         @sleep_count += 1
@@ -102,10 +115,10 @@ describe 'atom', type: :task do
       expect(@sleep_count).to eq(1)
     end
 
-    it "doesn't sleep if pause file not present" do
+    it 'doesn\'t sleep if pause file not present' do
       expect(File.exist?(pause_file)).to be_falsey # just to be sure
 
-      # hack to "expect().not_to receive" global sleep call
+      # HACK: to "expect().not_to receive" global sleep call
       @sleep_count = 0
       allow_any_instance_of(Object).to receive(:sleep).with(300) do
         @sleep_count += 1
@@ -118,7 +131,7 @@ describe 'atom', type: :task do
 
     # TODO: fix code, then re-enable this test
     skip 'exits without trying to parse if request returns 404' do
-      stub_request(:get, starting_point).to_return(status: [404, "Not Found"])
+      stub_request(:get, starting_point).to_return(status: [404, 'Not Found'])
       expect(Nokogiri).not_to receive(:XML)
       invoke_update!
     end
@@ -131,19 +144,91 @@ describe 'atom', type: :task do
     end
 
     it 'exits without updating if feed not updated since last harvest' do
-      feed_updated = DateTime.parse(feed_xml.at_xpath("//xmlns:updated").text)
+      feed_updated = DateTime.parse(feed_xml.at_xpath('//xmlns:updated').text)
       write_feeddate(feed_updated + 1) # +1 day
       expect(Mrt::Ingest::IObject).not_to receive(:new)
       invoke_update!
     end
 
     it 'updates if feed updated since last harvest' do
-      feed_updated = DateTime.parse(feed_xml.at_xpath("//xmlns:updated").text)
+      feed_updated = DateTime.parse(feed_xml.at_xpath('//xmlns:updated').text)
       write_feeddate(feed_updated - 1) # -1 day
 
       expect(server).to receive(:add_file).exactly(2).times
+      expect(client).to receive(:ingest).exactly(2).times
 
       invoke_update!
+    end
+
+    it 'stages the ERC kernel metadata via the one-time server' do
+      feed_updated = DateTime.parse(feed_xml.at_xpath('//xmlns:updated').text)
+      write_feeddate(feed_updated - 1) # -1 day
+
+      invoke_update!
+
+      expected0 = <<~TMP0
+        erc:
+        who: Merced County Sheriff's Office
+        what: Merced County Sheriff's Office mug book
+        when: 1885-19--?
+        where:
+        when/created:
+        when/modified: 2015-08-20T19:06:37.622000+00:00
+      TMP0
+
+      expected1 = <<~TMP1
+        erc:
+        who:
+        what: Merced Army Flying School
+        when: 194-?
+        where:
+        when/created:
+        when/modified: 2015-05-29T18:01:40.927000+00:00
+      TMP1
+
+      expected = [
+        expected0,
+        expected1
+      ]
+
+      temp_contents = @tempfiles.map { |f| File.read(f) }
+      expect(temp_contents).to eq(expected)
+    end
+
+    it 'correctly populates the ingest request' do
+      feed_updated = DateTime.parse(feed_xml.at_xpath('//xmlns:updated').text)
+      write_feeddate(feed_updated - 1) # -1 day
+
+      request_args = []
+      allow(client).to(receive(:ingest)) { |request| request_args << request.mk_args }
+
+      invoke_update!
+
+      expect(request_args.size).to eq(2)
+      files = request_args.map { |ra| ra['file'] }
+      # TODO: test file contents
+
+      expected_args = [
+        {
+          'file' => files[0],
+          'filename' => File.basename(files[0]),
+          'localIdentifier' => '494672cf-2937-4975-8b33-90bf80b4c8a6',
+          'profile' => 'example_ingest_profile',
+          'responseForm' => 'json',
+          'submitter' => 'Atom processor/Example U Digital Special Collections',
+          'type' => 'object-manifest'
+        },
+        {
+          'file' => files[1],
+          'filename' => File.basename(files[1]),
+          'localIdentifier' => '365579cc-a369-45e7-8977-047bda3f7ed1',
+          'profile' => 'example_ingest_profile',
+          'responseForm' => 'json',
+          'submitter' => 'Atom processor/Example U Digital Special Collections',
+          'type' => 'object-manifest'
+        }
+      ]
+      expect(request_args).to eq(expected_args)
     end
 
     # TODO: make this happen
@@ -151,7 +236,7 @@ describe 'atom', type: :task do
     pending 'exits with an error if ATOM_CONFIG does not include local ID element'
 
     pending 'stops paginating when next page is nil'
-    pending "doesn't update if initial feed is nil"
+    pending 'doesn\'t update if initial feed is nil'
 
     pending 'requires a submitter'
     pending 'requires a profile'
