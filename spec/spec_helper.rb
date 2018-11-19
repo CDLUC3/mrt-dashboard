a9f9667ba9bced206ebd37f06d2c507d4adfbb44
David Moles <david.moles@ucop.edu>
2018-11-19 12:05:21 -0800

    Add profiling output to specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,50 @@ if ENV['COVERAGE']
 end
 
 # ------------------------------------------------------------
+# Rudimentary performance profiling
+# cf. https://www.foraker.com/blog/profiling-your-rspec-suite
+require 'time'
+
+class ProfilingReporter
+  attr_reader :times_started
+  attr_reader :times_elapsed
+
+  def initialize
+    @times_started = {}
+    @times_elapsed = {}
+  end
+
+  def example_started(n10n)
+    times_started[desc(n10n)] = Time.now
+  end
+
+  def example_finished(n10n)
+    time_finished = Time.now
+    description = desc(n10n)
+    time_elapsed = (time_finished - times_started[description])
+    times_elapsed[description] = time_elapsed
+  end
+
+  def dump_summary(_n10n)
+    puts "\nTime(s)\tLocation\tExample"
+    times_elapsed.sort_by(&:last).reverse_each do |desc, time_elapsed|
+      puts "#{format('%.4f', time_elapsed)}\t#{desc}"
+    end
+  end
+
+  private
+
+  def desc(n10n)
+    example = n10n.example
+    md = example.metadata
+    path = md[:file_path].sub(%r{^\./}, '')
+    line_number = md[:line_number].to_s.ljust(3)
+    "#{path}:#{line_number}\t#{example.full_description}"
+  end
+
+end
+
+# ------------------------------------------------------------
 # Rspec configuration
 
 RSpec.configure do |config|
@@ -18,6 +62,7 @@ RSpec.configure do |config|
   config.formatter = :documentation
   # config.raise_errors_for_deprecations! # TODO: enable this
   config.mock_with :rspec
+  config.reporter.register_listener(ProfilingReporter.new, :dump_summary, :example_started, :example_finished)
 end
 
 require 'rspec_custom_matchers'
