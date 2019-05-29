76219ed0418d12ce9d9fcf6bddd9c53ed8cda02b
David Moles <david.moles@ucop.edu>
2019-05-29 10:27:40 -0700

    Fix Atom CSH script generated filenames

--- lib/merritt/atom/csh_generator.rb
+++ lib/merritt/atom/csh_generator.rb
@@ -7,47 +7,77 @@ module Merritt
     class CSHGenerator
       ARG_KEYS = %i[feed_url collection_ark collection_name collection_mnemonic].freeze
       ARK_QUALIFIER_REGEXP = %r{(?<=/)[^/]+$}
+      REGISTRY_ID_REGEXP = /(?<=_)[0-9]+(?=\.atom$)/
 
       CSH_TEMPLATE = <<~ERB.freeze
         # setenv RAILS_ENV stage
         setenv PATH /dpr2/local/bin:${PATH}
-        
+
         set date = `date +%Y%m%d`
         set base = /apps/dpr2/apps/ui/atom
-        
+
         cd /dpr2/apps/ui/current
-        
+
         # Nuxeo Collection
         #    Atom URL: <%= feed_url %>
+        #    Registry ID: <%= registry_id %>
+        #    Name: <%= nuxeo_collection_name %>
         #
         # Merritt Collection
-        #    Collection ID: <%= collection_ark %>
-        #    Name: <%= collection_name %>
-        #    Mnemonic: <%= collection_mnemonic %>
-        
+        #    Collection ID: <%= merritt_collection_ark %>
+        #    Name: <%= merritt_collection_name %>
+        #    Mnemonic: <%= merritt_collection_mnemonic %>
+
         # To pause, uncomment...
-        #touch PAUSE_ATOM_<%= collection_mnemonic %>_content
-        
+        #touch PAUSE_ATOM_<%= merritt_collection_mnemonic %>_content
+
         # Last feed update, defined in atom.ldap
-        # /dpr2/apps/ui/atom/LastUpdate/lastFeedUpdate_<%= collection_ark_qualifier %>
-        
+        # /dpr2/apps/ui/atom/LastUpdate/lastFeedUpdate_<%= registry_id %>-<%= collection_ark_qualifier %>
+
         set feedURL	= "<%= feed_url %>"
-        set userAgent	= "Atom processor/<%= collection_name %>"
-        set profile	= "<%= collection_mnemonic %>_content"
-        set collection	= "<%= collection_ark_qualifier %>"         # feed collection
-        set groupID	= "<%= collection_ark %>"
-        # Was defined in atom.yml file, but now here
-        set updateFile	= "/dpr2/apps/ui/atom/LastUpdate/lastFeedUpdate_${collection}"
+        set userAgent	= "Atom processor/<%= merritt_collection_name %>"
+        set profile	= "<%= merritt_collection_mnemonic %>_content"
+        set groupID	= "<%= merritt_collection_ark %>"
+        set updateFile	= "/dpr2/apps/ui/atom/LastUpdate/lastFeedUpdate_<%= registry_id %>-<%= collection_ark_qualifier %>"
         set log		= "${base}/logs/${profile}_${date}.log"
-        
+
         # Log file
         bundle exec rake "atom:update[${feedURL}, ${userAgent}, ${profile}, ${groupID}, ${updateFile}]" >& ${log} &
-        
+
         # No log file
         # bundle exec rake "atom:update[${feedURL}, ${userAgent}, ${profile}, ${groupID}, ${updateFile}]"
-        
+
         exit
       ERB
+
+      attr_reader :collection_ark_qualifier
+      attr_reader :registry_id
+
+      attr_reader :nuxeo_collection_name
+      attr_reader :feed_url
+      attr_reader :merritt_collection_mnemonic
+      attr_reader :merritt_collection_ark
+      attr_reader :merritt_collection_name
+
+      def initialize(nuxeo_collection_name:, feed_url:, merritt_collection_mnemonic:, merritt_collection_ark:, merritt_collection_name:)
+        @collection_ark_qualifier = merritt_collection_ark.scan(ARK_QUALIFIER_REGEXP).first
+        @registry_id = feed_url.scan(REGISTRY_ID_REGEXP).first
+
+        @nuxeo_collection_name = nuxeo_collection_name
+        @feed_url = feed_url
+        @merritt_collection_mnemonic = merritt_collection_mnemonic
+        @merritt_collection_ark = merritt_collection_ark
+        @merritt_collection_name = merritt_collection_name
+      end
+
+      def generate_csh
+        CSHGenerator.template.result(binding)
+      end
+
+      def filename
+        sanitized_name = CSHGenerator.sanitize_name(nuxeo_collection_name)
+        "#{registry_id}-#{sanitized_name}-#{merritt_collection_mnemonic}.csh"
+      end
 
       class << self
 
@@ -56,22 +86,31 @@ module Merritt
         end
 
         # noinspection RubyUnusedLocalVariable
-        def generate_csh(feed_url:, collection_mnemonic:, collection_ark:, collection_name:)
-          collection_ark_qualifier = collection_ark.scan(ARK_QUALIFIER_REGEXP).first
-          opts = OpenStruct.new(
+        def generate_csh(nuxeo_collection_name:, feed_url:, merritt_collection_mnemonic:, merritt_collection_ark:, merritt_collection_name:)
+          generator = CSHGenerator.new(
+            nuxeo_collection_name: nuxeo_collection_name,
             feed_url: feed_url,
-            collection_mnemonic: collection_mnemonic,
-            collection_ark: collection_ark,
-            collection_name: collection_name,
+            merritt_collection_mnemonic: merritt_collection_mnemonic,
+            merritt_collection_ark: merritt_collection_ark,
+            merritt_collection_name: merritt_collection_name
           )
-          template.result(binding)
+          generator.generate_csh
+        end
+
+        def sanitize_name(name)
+          name.gsub(/[^A-Za-z0-9]+/, '-').gsub(/-+%/, '').gsub(/([a-z])-s/, '\\1s')
         end
 
         def from_csv(csv_data:, to_dir:)
           CSV.parse(csv_data) do |row|
-            feed_url, collection_mnemonic, collection_ark, collection_name = row
-            csh = generate_csh(feed_url: feed_url, collection_mnemonic: collection_mnemonic, collection_ark: collection_ark, collection_name: collection_name)
-            File.open(File.join(to_dir, "#{collection_mnemonic}.csh"), 'w') { |f| f.write(csh) }
+            generator = CSHGenerator.new(
+              nuxeo_collection_name: row[0],
+              feed_url: row[1],
+              merritt_collection_mnemonic: row[2],
+              merritt_collection_ark: row[3],
+              merritt_collection_name: row[4]
+            )
+            File.open(File.join(to_dir, generator.filename), 'w') { |f| f.write(generator.generate_csh) }
           end
         end
 
--- spec/libs/merritt/atom/csh_generator_spec.rb
+++ spec/libs/merritt/atom/csh_generator_spec.rb
@@ -10,49 +10,50 @@ module Merritt
           expected_csh = <<~CSH
             # setenv RAILS_ENV stage
             setenv PATH /dpr2/local/bin:${PATH}
-            
+
             set date = `date +%Y%m%d`
             set base = /apps/dpr2/apps/ui/atom
-            
+
             cd /dpr2/apps/ui/current
-            
+
             # Nuxeo Collection
             #    Atom URL: https://s3.amazonaws.com/static.ucldc.cdlib.org/merritt/ucldc_collection_26098.atom
+            #    Registry ID: 26098
+            #    Name: UCM Ramicova
             #
             # Merritt Collection
             #    Collection ID: ark:/13030/m5b58sn8
             #    Name: Merced Library Nuxeo collection
             #    Mnemonic: ucm_lib_nuxeo
-            
+
             # To pause, uncomment...
             #touch PAUSE_ATOM_ucm_lib_nuxeo_content
-            
+
             # Last feed update, defined in atom.ldap
-            # /dpr2/apps/ui/atom/LastUpdate/lastFeedUpdate_m5b58sn8
-            
+            # /dpr2/apps/ui/atom/LastUpdate/lastFeedUpdate_26098-m5b58sn8
+
             set feedURL	= "https://s3.amazonaws.com/static.ucldc.cdlib.org/merritt/ucldc_collection_26098.atom"
             set userAgent	= "Atom processor/Merced Library Nuxeo collection"
             set profile	= "ucm_lib_nuxeo_content"
-            set collection	= "m5b58sn8"         # feed collection
             set groupID	= "ark:/13030/m5b58sn8"
-            # Was defined in atom.yml file, but now here
-            set updateFile	= "/dpr2/apps/ui/atom/LastUpdate/lastFeedUpdate_${collection}"
+            set updateFile	= "/dpr2/apps/ui/atom/LastUpdate/lastFeedUpdate_26098-m5b58sn8"
             set log		= "${base}/logs/${profile}_${date}.log"
-            
+
             # Log file
             bundle exec rake "atom:update[${feedURL}, ${userAgent}, ${profile}, ${groupID}, ${updateFile}]" >& ${log} &
-            
+
             # No log file
             # bundle exec rake "atom:update[${feedURL}, ${userAgent}, ${profile}, ${groupID}, ${updateFile}]"
-            
+
             exit
           CSH
 
           actual_csh = CSHGenerator.generate_csh(
             feed_url: 'https://s3.amazonaws.com/static.ucldc.cdlib.org/merritt/ucldc_collection_26098.atom',
-            collection_ark: 'ark:/13030/m5b58sn8',
-            collection_name: 'Merced Library Nuxeo collection',
-            collection_mnemonic: 'ucm_lib_nuxeo'
+            merritt_collection_ark: 'ark:/13030/m5b58sn8',
+            merritt_collection_name: 'Merced Library Nuxeo collection',
+            merritt_collection_mnemonic: 'ucm_lib_nuxeo',
+            nuxeo_collection_name: 'UCM Ramicova'
           )
 
           File.open('tmp/expected.csh', 'w') { |f| f.write(expected_csh) }
@@ -62,32 +63,72 @@ module Merritt
         end
       end
 
+      describe :sanitize_name do
+        it 'sanitizes names' do
+          names = {
+            'Raebel, Hermann C. Papers' => 'Raebel-Hermann-C-Papers',
+            'UCCE Merced' => 'UCCE-Merced',
+            'LIJA/Clark Center' => 'LIJA-Clark-Center',
+            'McLean' => 'McLean',
+            'Angel’s Camp' => 'Angels-Camp',
+            'Miriam Matthews Photograph Collection' => 'Miriam-Matthews-Photograph-Collection',
+            '' => ''
+          }
+          names.each do |original, expected|
+            actual = CSHGenerator.sanitize_name(original)
+            expect(actual).to eq(expected)
+          end
+        end
+      end
+
       describe :from_csh do
         it 'generates a file for each entry' do
           csv_data = <<~CSV
-            https://s3.amazonaws.com/static.ucldc.cdlib.org/merritt/ucldc_collection_27014.atom,ucm_lib_ucce_humboldt,ark:/13030/m590717c,UC Merced Library UCCE Humboldt County
-            https://s3.amazonaws.com/static.ucldc.cdlib.org/merritt/ucldc_collection_27013.atom,ucm_lib_ucce_ventura,ark:/13030/m5dr7rw3,UC Merced Library UCCE Ventura County
-            https://s3.amazonaws.com/static.ucldc.cdlib.org/merritt/ucldc_collection_27012.atom,ucm_lib_ucce_merced,ark:/13030/m5jh8hmt,UC Merced Library UCCE Merced County
-            https://s3.amazonaws.com/static.ucldc.cdlib.org/merritt/ucldc_collection_65.atom,ucm_lib_clark,ark:/13030/m5n58jd1,UCM Library Clark Center for Japanese Art and Culture
-            https://s3.amazonaws.com/static.ucldc.cdlib.org/merritt/ucldc_collection_68.atom,ucm_lib_mclean,ark:/13030/m5p89893,UC Merced Library McLean Collection
-            https://s3.amazonaws.com/static.ucldc.cdlib.org/merritt/ucldc_collection_14256.atom,ucm_lib_mcdaniel,ark:/13030/m5t20138,UC Merced Library McDaniel (Wilma E.) Papers
-            https://s3.amazonaws.com/static.ucldc.cdlib.org/merritt/ucldc_collection_69.atom,ucm_lib_acm,ark:/13030/m5xq22b2,UC Merced Library Angels Camp Museum
+            UCCE Humboldt,https://s3.amazonaws.com/static.ucldc.cdlib.org/merritt/ucldc_collection_27014.atom,ucm_lib_ucce_humboldt,ark:/13030/m590717c,UC Merced Library UCCE Humboldt County,
+            UCCE Ventura,https://s3.amazonaws.com/static.ucldc.cdlib.org/merritt/ucldc_collection_27013.atom,ucm_lib_ucce_ventura,ark:/13030/m5dr7rw3,UC Merced Library UCCE Ventura County,
+            UCCE Merced,https://s3.amazonaws.com/static.ucldc.cdlib.org/merritt/ucldc_collection_27012.atom,ucm_lib_ucce_merced,ark:/13030/m5jh8hmt,UC Merced Library UCCE Merced County,
+            LIJA/Clark Center,https://s3.amazonaws.com/static.ucldc.cdlib.org/merritt/ucldc_collection_65.atom,ucm_lib_clark,ark:/13030/m5n58jd1,UCM Library Clark Center for Japanese Art and Culture,Also exists on stage
+            McLean,https://s3.amazonaws.com/static.ucldc.cdlib.org/merritt/ucldc_collection_68.atom,ucm_lib_mclean,ark:/13030/m5p89893,UC Merced Library McLean Collection,
+            McDaniel,https://s3.amazonaws.com/static.ucldc.cdlib.org/merritt/ucldc_collection_14256.atom,ucm_lib_mcdaniel,ark:/13030/m5t20138,UC Merced Library McDaniel (Wilma E.) Papers,
+            Angel’s Camp,https://s3.amazonaws.com/static.ucldc.cdlib.org/merritt/ucldc_collection_69.atom,ucm_lib_acm,ark:/13030/m5xq22b2,UC Merced Library Angels Camp Museum,
+            "Raebel, Hermann C. Papers",https://s3.amazonaws.com/static.ucldc.cdlib.org/merritt/ucldc_collection_26899.atom,ucla_digital_lib,ark:/13030/m5k40smm,UCLA Digital Library Program,"Exists on stage only, for the UCLA pilot"
+            Miriam Matthews Photograph Collection,https://s3.amazonaws.com/static.ucldc.cdlib.org/merritt/ucldc_collection_26936.atom,ucla_digital_lib,ark:/13030/m5k40smm,UCLA Digital Library Program,"Exists on stage only, for the UCLA pilot"
           CSV
+
+          expected_files = %w[
+            27014-UCCE-Humboldt-ucm_lib_ucce_humboldt.csh
+            27013-UCCE-Ventura-ucm_lib_ucce_ventura.csh
+            27012-UCCE-Merced-ucm_lib_ucce_merced.csh
+            65-LIJA-Clark-Center-ucm_lib_clark.csh
+            68-McLean-ucm_lib_mclean.csh
+            14256-McDaniel-ucm_lib_mcdaniel.csh
+            69-Angels-Camp-ucm_lib_acm.csh
+            26899-Raebel-Hermann-C-Papers-ucla_digital_lib.csh
+            26936-Miriam-Matthews-Photograph-Collection-ucla_digital_lib.csh
+          ]
 
           Dir.mktmpdir do |tmpdir|
             CSHGenerator.from_csv(csv_data: csv_data, to_dir: tmpdir)
             files = Dir.entries(tmpdir)
-                      .map { |f| File.join(tmpdir, f) }
-                      .select { |f| File.file?(f) }
+              .map { |f| File.join(tmpdir, f) }
+              .select { |f| File.file?(f) }
+            index = 0
             CSV.parse(csv_data) do |row|
-              feed_url, collection_mnemonic, collection_ark, collection_name = row
-              expected_file = File.join(tmpdir, "#{collection_mnemonic}.csh")
+              nuxeo_collection_name, feed_url, collection_mnemonic, collection_ark, merritt_collection_name = row[0...5]
+              expected_file = File.join(tmpdir, expected_files[index])
               expect(files).to include(expected_file)
-              expected_data = CSHGenerator.generate_csh(feed_url: feed_url, collection_mnemonic: collection_mnemonic, collection_ark: collection_ark, collection_name: collection_name)
+              expected_data = CSHGenerator.generate_csh(
+                nuxeo_collection_name: nuxeo_collection_name,
+                feed_url: feed_url,
+                merritt_collection_mnemonic: collection_mnemonic,
+                merritt_collection_ark: collection_ark,
+                merritt_collection_name: merritt_collection_name
+              )
               actual_data = File.read(expected_file)
               expect(actual_data).to eq(expected_data)
+              index += 1
             end
-            expect(files.size).to eq(7)
+            expect(files.size).to eq(expected_files.size)
           end
         end
       end
