ad2582498386a09ae825e4182f34ff317500e595
Terry Brady <terrywbrady@gmail.com>
2020-03-26 16:52:21 -0700

    Update downloads_controller.rb

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -7,7 +7,7 @@ class DownloadsController < ApplicationController
     if params.key?('available')
       redirect_to("https://merritt-stage.cdlib.org/d/ark%253A%252F99999%252Ffk4g46174f")
     else
-      render(file: 'app/views/downloads/index.html.erb')
+      redirect_to("/downloads")
     end
   end
 end
