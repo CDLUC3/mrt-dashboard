0c9a86edb34e3f2f54c13c0147684d2877ed764d
Terry Brady <terrywbrady@gmail.com>
2020-03-26 16:36:59 -0700

    Add simulation

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,2 +1,14 @@
 class DownloadsController < ApplicationController
+  def add()
+    render(file: 'app/views/downloads/index.html.erb')
+  end
+
+  def get()
+    puts(params)
+    if params.key?('available')
+      redirect_to("https://merritt-stage.cdlib.org/d/ark%253A%252F99999%252Ffk4g46174f")
+    else
+      render(file: 'app/views/downloads/index.html.erb')
+    end
+  end
 end
