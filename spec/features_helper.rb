0f4c213caeac715801eb2500092fc0943c5db368
Terry Brady <terrywbrady@gmail.com>
2020-04-16 16:23:47 -0700

    Update features_helper.rb

--- spec/features_helper.rb
+++ spec/features_helper.rb
@@ -27,13 +27,12 @@ end
 # end
 
 Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: {
-      args: %w[headless disable-gpu]
-    }
-  )
+  caps = Selenium::WebDriver::Remote::Capabilities.chrome(loggingPrefs: { browser: 'ALL' })
+  opts = Selenium::WebDriver::Chrome::Options.new
 
-  Capybara::Selenium::Driver.new app, browser: :chrome, desired_capabilities: capabilities
+  chrome_args = %w[--headless --no-sandbox --disable-gpu --window-size=1920,1080 --remote-debugging-port=9222]
+  chrome_args.each { |arg| opts.add_argument(arg) }
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: opts, desired_capabilities: caps)
 end
 
 Capybara.javascript_driver = :headless_chrome
