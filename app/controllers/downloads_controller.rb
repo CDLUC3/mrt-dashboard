c3830ddb12a5dbb799d0694adbb5a993c36dba96
Terry Brady <terrywbrady@gmail.com>
2020-04-23 16:56:20 -0700

    add respond_to

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -3,7 +3,19 @@ class DownloadsController < ApplicationController
     render(file: 'app/views/downloads/index.html.erb')
   end
 
+
+  def index
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   def get
+    respond_to do |format|
+      format.html
+      format.json
+    end
     do_presign_obj_by_token(params[:token], params[:no_redirect])
   end
 end
