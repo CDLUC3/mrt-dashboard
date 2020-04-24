b0fbbc9ad50084da92e5f1fd0e9c9a37575d56f3
Terry Brady <terrywbrady@gmail.com>
2020-04-23 17:01:59 -0700

    Update downloads_controller.rb

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,21 +1,17 @@
 class DownloadsController < ApplicationController
   def add
+    @tasks = Task.all
+    respond_to :html, :json
     render(file: 'app/views/downloads/index.html.erb')
   end
 
 
   def index
-    respond_to do |format|
-      format.html
-      format.json
-    end
+    @tasks = Task.all
+    respond_to :html, :json
   end
 
   def get
-    respond_to do |format|
-      format.html
-      format.json
-    end
     do_presign_obj_by_token(params[:token], params[:no_redirect])
   end
 end
