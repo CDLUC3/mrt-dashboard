e453ca77b6772f1208c76f7c2e9ef3e8cfa752bb
Terry Brady <terrywbrady@gmail.com>
2020-04-24 14:21:52 -0700

    js precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,6 @@
-Rails.application.config.assets.precompile += %w( jquery_ujs.js )
-Rails.application.config.assets.precompile += %w( jquery.js )
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
+
+Rails.application.config.assets.precompile += %w[
+  jquery_ujs.js,
+  jquery.js
+ ]
