1bdc18ee3ce866f06e8d9e4415413e6921275dae
Terry Brady <terrywbrady@gmail.com>
2020-04-24 14:08:06 -0700

    add js deps

---
+++ public/javascripts/presign_download_page.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  var cmd = getCommand();
+  var token = getToken();
+  if (cmd == "add") {
+    addToken(token);
+    document.location = "/downloads";
+  }
+  showTable();
+});
