28cf009b5213162ffa6dd7aeb0a0e5dad1a1c709
Terry Brady <terrywbrady@gmail.com>
2020-04-28 16:27:32 -0700

    update jquery include

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,7 @@
 Rails.application.config.assets.precompile += %w( presign_download.js )
 Rails.application.config.assets.precompile += %w( presign_download_page.js )
+Rails.application.config.assets.precompile += %w( jquery.min.js )
+Rails.application.config.assets.precompile += %w( jquery-ui.js )
+
 Rails.application.config.assets.precompile += %w( presign_download.css )
+Rails.application.config.assets.precompile += %w( jquery-ui.css )
