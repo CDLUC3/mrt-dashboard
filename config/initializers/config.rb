278db67d9c16ffe4e794425bbe1c585ba7c4ada0
Terry Brady <terrywbrady@gmail.com>
2020-08-11 11:20:19 -0700

    properly escape vars

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -3,7 +3,9 @@ require 'uc3-ssm'
 def load_config(name)
   path = File.join(Rails.root, 'config', name)
   conf = Uc3Ssm::ConfigResolver.new.resolve_file_values(path)
+  puts conf
   conf_env = conf[Rails.env]
+  puts conf_env
   conf_env.class == String ? conf[conf_env] : conf_env
 end
 
@@ -12,4 +14,7 @@ ATOM_CONFIG = load_config('atom.yml')
 APP_CONFIG = load_config('app_config.yml')
 SSM_ENV = load_config('ssm-env.yml')
 
+puts "TBTB ***"
 puts SSM_ENV
+puts "TBTB ***"
+puts Rails.env
