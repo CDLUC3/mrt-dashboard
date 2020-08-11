dd502116332fa3e4e68c978f0ca548813fd4c8a4
Terry Brady <terrywbrady@gmail.com>
2020-08-11 10:40:30 -0700

    undo

--- config/application.rb
+++ config/application.rb
@@ -65,7 +65,6 @@ module MrtDashboard
     config.assets.enabled = false
 
     def config.database_configuration
-      ENV['SSM_ROOT_PATH'] = '/uc3/mrt/stg/'
       myconfig = super
       myconfig = Uc3Ssm::ConfigResolver.new.resolve_hash_values(myconfig)
       myconfig
