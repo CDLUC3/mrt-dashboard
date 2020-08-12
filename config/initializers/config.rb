650b693dce510cd398559662bc2a0717e36730a3
Terry Brady <terrywbrady@gmail.com>
2020-08-11 18:12:11 -0700

    Update config.rb

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -3,6 +3,7 @@ require 'uc3-ssm'
 def load_config(name)
   path = File.join(Rails.root, 'config', name)
   conf = Uc3Ssm::ConfigResolver.new("NOT_APPLICABLE", "us-west-2", "/uc3/mrt/stg/").resolve_file_values(path)
+  puts conf
   conf_env = conf[Rails.env]
   conf_env.class == String ? conf[conf_env] : conf_env
 end
@@ -11,7 +12,7 @@ LDAP_CONFIG = load_config('ldap.yml')
 ATOM_CONFIG = load_config('atom.yml')
 APP_CONFIG = load_config('app_config.yml')
 dbc = load_config('database.yml')
-puts "TBTB ***"
+puts "TBTB *** #{Rails.env}"
 puts dbc
 
 ENV['DATABASE_URL']="#{dbc['adapter']}://#{dbc['username']}:#{dbc['password']}@#{dbc['host']}:#{dbc['port']}/#{dbc['database']}"
