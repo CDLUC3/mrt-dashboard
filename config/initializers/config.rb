a06138b9e6c6a4526efd564ea36dd18f167035bd
Terry Brady <terrywbrady@gmail.com>
2020-08-12 17:07:25 -0700

    Update config.rb

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -4,11 +4,13 @@ require 'uc3-ssm'
 # resolve_key - partially process config file using this as a root key - use this to prevent unnecessary lookups
 # return_key - return values for a specific hash key - use this to filter the return object
 def load_uc3_config(name:, resolve_key: nil, return_key: nil)
-  resolver = Uc3Ssm::ConfigResolver.new({
-        def_value: "NOT_APPLICABLE",
-        region: ENV.key?('AWS_REGION') ? ENV['AWS_REGION'] : "us-west-2",
-        ssm_root_path: ENV.key?('SSM_ROOT_PATH') ? ENV['SSM_ROOT_PATH'] : "/uc3/mrt/stg/"
-    })
+  resolver = Uc3Ssm::ConfigResolver.new(
+    {
+      def_value: 'NOT_APPLICABLE',
+      region: ENV.key?('AWS_REGION') ? ENV['AWS_REGION'] : 'us-west-2',
+      ssm_root_path: ENV.key?('SSM_ROOT_PATH') ? ENV['SSM_ROOT_PATH'] : '/uc3/mrt/stg/'
+    }
+  )
   path = File.join(Rails.root, 'config', name)
   resolver.resolve_file_values(file: path, resolve_key: resolve_key, return_key: return_key)
 end
