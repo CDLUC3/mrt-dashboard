4c40ee9f16217941ffa94d3ed65327c68936c104
Terry Brady <terrywbrady@gmail.com>
2020-09-01 16:04:03 -0700

    enable keyboard controls

--- app/assets/javascripts/presign_download_page.js
+++ app/assets/javascripts/presign_download_page.js
@@ -48,4 +48,21 @@ jQuery(document).ready(function(){
     presignDialogs.assemblyTokenList.clearData();
     return true;
   })
+  jQuery("header nav li.dropdown a.expandable, header nav li.dropdown span.expandable")
+    .on('keypress', function(e) {
+    if (e.which == 32) {
+        jQuery(this).attr("aria-label", "Press spacebar to expand/contract the menu");
+        jQuery(this).parent("li").toggleClass("menuopen");
+        return false;
+    }
+  });
+  jQuery("header nav img.expandable")
+    .on('keypress', function(e) {
+    if (e.which == 32) {
+        jQuery(this).attr("aria-label", "Press spacebar to expand/contract the menu");
+        jQuery("nav.menu > ul").toggle();
+        return false;
+    }
+  });
+
 });
