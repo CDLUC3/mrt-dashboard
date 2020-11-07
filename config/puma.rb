481bc7102b456870ac60fa2a6147bff79f1ecddc
Terry Brady <tbrady@uc3-mrtdocker03x2-dev.cdlib.org>
2020-11-06 17:52:18 -0800

    rubocop warning

--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,7 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch('PORT', 26181)
+port        ENV.fetch('PORT', 26_181)
 
 # Specifies the `environment` that Puma will run in.
 #
