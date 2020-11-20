91775e3530adad7d206deed0e978ac505cea6e06
Terry Brady <terrywbrady@gmail.com>
2020-11-19 16:56:10 -0800

    update encode library

--- app/lib/encoder.rb
+++ app/lib/encoder.rb
@@ -1,16 +1,11 @@
+require 'erb'
 module Encoder
 
   def self.urlencode(item)
-    # was URI.escape(item, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
-    res = ''
-    item.split.each do |c|
-      res += '%20' if res != ''
-      res += URI.encode_www_form_component(c)
-    end
-    res
+    ERB::Util.url_encode(item)
   end
 
   def self.urlunencode(item)
-    URI.decode_www_form_component(item)
+    URI.unescape(item)
   end
 end
