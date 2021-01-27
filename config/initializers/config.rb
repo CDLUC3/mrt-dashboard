922939d46a686209f4e81a2034fe7fb129679c5a
Terry Brady <terrence.brady@ucop.edu>
2021-01-26 17:12:51 -0800

    simplify ssm init

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,13 +1,10 @@
 require 'uc3-ssm'
-require 'socket'
 
 # name - config file to process
 # return_key - return values for a specific hash key - use this to filter the return object
 def load_uc3_config(name:, return_key: nil)
   resolver = Uc3Ssm::ConfigResolver.new(
-    def_value: 'NOT_APPLICABLE',
-    region: ENV.key?('AWS_REGION') ? ENV['AWS_REGION'] : 'us-west-2',
-    ssm_root_path: ENV.key?('SSM_ROOT_PATH') ? ENV['SSM_ROOT_PATH'] : '/uc3/mrt/dev/'
+    def_value: 'NOT_APPLICABLE'
   )
   path = File.join(Rails.root, 'config', name)
   resolver.resolve_file_values(file: path, return_key: return_key)
