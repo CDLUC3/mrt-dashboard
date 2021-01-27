9ffeb5acfb424e5c8846e679110e76b1f5a39877
Terry Brady <terrence.brady@ucop.edu>
2021-01-26 17:19:02 -0800

    env.fetch

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -3,7 +3,10 @@ require 'uc3-ssm'
 # name - config file to process
 # return_key - return values for a specific hash key - use this to filter the return object
 def load_uc3_config(name:, return_key: nil)
-  resolver = Uc3Ssm::ConfigResolver.new
+  resolver = Uc3Ssm::ConfigResolver.new(
+    def_value: 'NOT_APPLICABLE',
+    ssm_root_path: ENV.fetch('SSM_ROOT_PATH', '/uc3/mrt/na/')
+  )
   path = File.join(Rails.root, 'config', name)
   resolver.resolve_file_values(file: path, return_key: return_key)
 end
