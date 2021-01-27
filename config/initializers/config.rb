350243155475c03fc291d4ff634cdd4da59500ea
Terry Brady <terrence.brady@ucop.edu>
2021-01-27 10:06:34 -0800

    Note SSM_ROOT_PATH dependencies

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,16 +1,11 @@
 APP_VERSION = File.exist?('.version') ? File.read('.version').chop.chop.chomp(';') : 'no-deploy-tag'
 
-if ENV.fetch('SSM_ROOT_PATH', '').empty?
-  puts ' *** SSM_ROOT_PATH is empty, will set based on Rails.env'
-  ENV['SSM_ROOT_PATH'] = case Rails.env
-                         when 'production'
-                           '/uc3/mrt/prd/'
-                         when 'stage'
-                           '/uc3/mrt/stg/'
-                         else
-                           '/uc3/mrt/dev/'
-                         end
-end
+# When running outside of a local/docker environment, the SSM_ROOT_PATH must be set
+# - For Capistrano, this is set in ~/.bashrc
+# - For Systemd, this is set in 'Environment'
+# - For docker deployments, this can be passed in via docker-compose. 
+#   - SSM_SKIP_RESOLUTION indicates that all values will be resolved from the EVN
+raise ' *** SSM_ROOT_PATH is empty' if ENV.fetch('SSM_ROOT_PATH', '').empty? && ENV.fetch('SSM_SKIP_RESOLUTION', '').empty?
 
 LDAP_CONFIG = Uc3Ssm::ConfigResolver.new({ def_value: 'NOT_APPLICABLE' }).resolve_file_values({ file: 'config/ldap.yml', return_key: Rails.env })
 ATOM_CONFIG = Uc3Ssm::ConfigResolver.new.resolve_file_values({ file: 'config/atom.yml', return_key: Rails.env })
