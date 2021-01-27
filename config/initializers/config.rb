5d1a2986d0399bc87738d6a79da4168f64e0db91
Terry Brady <terrence.brady@ucop.edu>
2021-01-26 21:00:07 -0800

    def

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -6,7 +6,7 @@ if ENV.empty? || ENV.nil?
   ATOM_CONFIG = {}
   APP_CONFIG = {}
 else
-  puts "Set ---- #{ENV}"
+  puts "Set ---- #{ENV.class}: #{ENV}"
   LDAP_CONFIG = Uc3Ssm::ConfigResolver.new({def_value: 'NOT_APPLICABLE'}).resolve_file_values({file: 'config/ldap.yml', return_key: Rails.env})
   ATOM_CONFIG = Uc3Ssm::ConfigResolver.new.resolve_file_values({file: 'config/atom.yml', return_key: Rails.env})
   APP_CONFIG = Uc3Ssm::ConfigResolver.new.resolve_file_values({file: 'config/app_config.yml', return_key: Rails.env})
