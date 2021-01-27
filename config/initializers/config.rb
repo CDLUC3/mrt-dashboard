bf8e1e0d8a12ba3da034e44b55d1de33ec77ef53
Terry Brady <terrence.brady@ucop.edu>
2021-01-26 22:38:21 -0800

    rubocop tidy

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,17 +1,17 @@
-APP_VERSION = File.exist?('.version') ? File.read('.version').chop.chop.chomp(";") : 'no-deploy-tag'
+APP_VERSION = File.exist?('.version') ? File.read('.version').chop.chop.chomp(';') : 'no-deploy-tag'
 
 if ENV.fetch('SSM_ROOT_PATH', '').empty?
-  puts " *** SSM_ROOT_PATH is empty, will set based on Rails.env"
-  if Rails.env == 'production'
-    ENV['SSM_ROOT_PATH'] = '/uc3/mrt/prd/'
-  elsif Rails.env == 'stage'
-    ENV['SSM_ROOT_PATH'] = '/uc3/mrt/stg/'
-  else Rails.env == 'dev'
-    ENV['SSM_ROOT_PATH'] = '/uc3/mrt/dev/'
-  end
+  puts ' *** SSM_ROOT_PATH is empty, will set based on Rails.env'
+  ENV['SSM_ROOT_PATH'] = case Rails.env
+                         when 'production'
+                           '/uc3/mrt/prd/'
+                         when 'stage'
+                           '/uc3/mrt/stg/'
+                         else
+                           '/uc3/mrt/dev/'
+                         end
 end
 
-LDAP_CONFIG = Uc3Ssm::ConfigResolver.new({def_value: 'NOT_APPLICABLE'}).resolve_file_values({file: 'config/ldap.yml', return_key: Rails.env})
-ATOM_CONFIG = Uc3Ssm::ConfigResolver.new.resolve_file_values({file: 'config/atom.yml', return_key: Rails.env})
-APP_CONFIG = Uc3Ssm::ConfigResolver.new.resolve_file_values({file: 'config/app_config.yml', return_key: Rails.env})
-  
+LDAP_CONFIG = Uc3Ssm::ConfigResolver.new({ def_value: 'NOT_APPLICABLE' }).resolve_file_values({ file: 'config/ldap.yml', return_key: Rails.env })
+ATOM_CONFIG = Uc3Ssm::ConfigResolver.new.resolve_file_values({ file: 'config/atom.yml', return_key: Rails.env })
+APP_CONFIG = Uc3Ssm::ConfigResolver.new.resolve_file_values({ file: 'config/app_config.yml', return_key: Rails.env })
