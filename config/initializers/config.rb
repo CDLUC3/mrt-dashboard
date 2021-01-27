677c8443ef66a29d2bc33ae3f2a088cd1eec80b0
Terry Brady <terrence.brady@ucop.edu>
2021-01-26 22:00:03 -0800

    def

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,6 +1,6 @@
 APP_VERSION = File.exist?('.version') ? File.read('.version').chop.chomp(';') : 'no-deploy-tag'
 
-raise "Environment var 'SSM_ROOT_PATH' not defined" unless ENV['SSM_ROOT_PATH']
+raise "Environment var 'SSM_ROOT_PATH' not defined" if ENV.fetch('SSM_ROOT_PATH', '').empty?
 
 LDAP_CONFIG = Uc3Ssm::ConfigResolver.new({def_value: 'NOT_APPLICABLE'}).resolve_file_values({file: 'config/ldap.yml', return_key: Rails.env})
 ATOM_CONFIG = Uc3Ssm::ConfigResolver.new.resolve_file_values({file: 'config/atom.yml', return_key: Rails.env})
