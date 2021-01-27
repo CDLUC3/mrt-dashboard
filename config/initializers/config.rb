adb9cc53e389a81ac8ea603b564c79b5a1cbf235
Terry Brady <terrence.brady@ucop.edu>
2021-01-26 17:30:57 -0800

    force os call for env

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -3,20 +3,14 @@ require 'uc3-ssm'
 # name - config file to process
 # return_key - return values for a specific hash key - use this to filter the return object
 def load_uc3_config(name:, return_key: nil)
-  puts "TBTB"
-  puts ENV
-  puts "TBTB"
+  # NOTE: ENV does not seemed to be initialized at this time
   resolver = Uc3Ssm::ConfigResolver.new(
     def_value: 'NOT_APPLICABLE',
-    ssm_root_path: ENV.fetch('SSM_ROOT_PATH', '/uc3/mrt/na/')
+    ssm_root_path: `echo $SSM_ROOT_PATH`.chomp
   )
   path = File.join(Rails.root, 'config', name)
   resolver.resolve_file_values(file: path, return_key: return_key)
 end
-
-puts "TBTB2"
-puts ENV
-puts "TBTB2"
 
 LDAP_CONFIG = load_uc3_config(name: 'ldap.yml', return_key: Rails.env)
 ATOM_CONFIG = load_uc3_config(name: 'atom.yml', return_key: Rails.env)
