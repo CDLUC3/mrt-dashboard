a3cde1e99eb7990304f3acf2f3de0c665bb78857
Terry Brady <terrence.brady@ucop.edu>
2021-01-26 20:54:07 -0800

    def

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,14 +1,14 @@
 APP_VERSION = File.exist?('.version') ? File.read('.version').chop.chomp(';') : 'no-deploy-tag'
 
-if ENV
-  puts "Set ---- #{ENV}"
-  LDAP_CONFIG = Uc3Ssm::ConfigResolver.new({def_value: 'NOT_APPLICABLE'}).resolve_file_values({file: 'config/ldap.yml', return_key: Rails.env})
-  ATOM_CONFIG = Uc3Ssm::ConfigResolver.new.resolve_file_values({file: 'config/atom.yml', return_key: Rails.env})
-  APP_CONFIG = Uc3Ssm::ConfigResolver.new.resolve_file_values({file: 'config/app_config.yml', return_key: Rails.env})
-else
+if ENV.empty?
   puts "default: #{ENV}"
   LDAP_CONFIG = {}
   ATOM_CONFIG = {}
   APP_CONFIG = {}
+else
+  puts "Set ---- #{ENV}"
+  LDAP_CONFIG = Uc3Ssm::ConfigResolver.new({def_value: 'NOT_APPLICABLE'}).resolve_file_values({file: 'config/ldap.yml', return_key: Rails.env})
+  ATOM_CONFIG = Uc3Ssm::ConfigResolver.new.resolve_file_values({file: 'config/atom.yml', return_key: Rails.env})
+  APP_CONFIG = Uc3Ssm::ConfigResolver.new.resolve_file_values({file: 'config/app_config.yml', return_key: Rails.env})
 end
   
