145246c0135b222c5a969a8f1180dcf5353bb630
Terry Brady <terrence.brady@ucop.edu>
2021-01-26 20:49:06 -0800

    conditional property init

--- config/application.rb
+++ config/application.rb
@@ -12,11 +12,6 @@ module MrtDashboard
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.0
 
-    puts "**** #{ENV}"
-    @ldap_config = Uc3Ssm::ConfigResolver.new({def_value: 'NOT_APPLICABLE'}).resolve_file_values({file: 'config/ldap.yml', return_key: Rails.env})
-    @atom_config = Uc3Ssm::ConfigResolver.new.resolve_file_values({file: 'config/atom.yml', return_key: Rails.env})
-    @app_config = Uc3Ssm::ConfigResolver.new.resolve_file_values({file: 'config/app_config.yml', return_key: Rails.env})
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
