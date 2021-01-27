7b5eb8a4b766a4114f0d91292d991aea5ad8164c
Terry Brady <terrence.brady@ucop.edu>
2021-01-26 22:22:21 -0800

    hardcode path

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,6 +1,15 @@
 APP_VERSION = File.exist?('.version') ? File.read('.version').chop.chomp(';') : 'no-deploy-tag'
 
-raise "Environment var 'SSM_ROOT_PATH' not defined" if ENV.fetch('SSM_ROOT_PATH', '').empty?
+if ENV.fetch('SSM_ROOT_PATH', '').empty?
+  puts " *** SSM_ROOT_PATH is empty"
+  if Rails.env == 'production'
+    ENV['SSM_ROOT_PATH'] = '/uc3/mrt/prd/'
+  elsif Rails.env == 'stage'
+    ENV['SSM_ROOT_PATH'] = '/uc3/mrt/stg/'
+  else Rails.env == 'dev'
+    ENV['SSM_ROOT_PATH'] = '/uc3/mrt/dev/'
+  end
+end
 
 LDAP_CONFIG = Uc3Ssm::ConfigResolver.new({def_value: 'NOT_APPLICABLE'}).resolve_file_values({file: 'config/ldap.yml', return_key: Rails.env})
 ATOM_CONFIG = Uc3Ssm::ConfigResolver.new.resolve_file_values({file: 'config/atom.yml', return_key: Rails.env})
