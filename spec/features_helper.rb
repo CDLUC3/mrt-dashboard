def0a6b1852b1326586adef3f367f2c0294ffd88
Terry Brady <terrence.brady@ucop.edu>
2023-08-29 17:21:25 -0700

    specify chrome version

--- spec/features_helper.rb
+++ spec/features_helper.rb
@@ -41,6 +41,8 @@ RSpec.configure do |config|
   config.before(:each, type: :feature, js: true) do
     Webdrivers.cache_time = 0
     Capybara.current_driver = :selenium_chrome_headless
+    # the following is needed for github actions
+    Webdrivers::Chromedriver.required_version = '114.0.5735.90'
     # Toggle the following when needed during testing
     # Capybara.current_driver = :selenium_chrome
   end
