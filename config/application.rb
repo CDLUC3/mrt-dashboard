6de3341ab6eba4b7efcefdf8da481ab7befcc7b7
Terry Brady <terrence.brady@ucop.edu>
2024-11-13 17:46:40 -0800

    setting changes for docker run

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,8 @@ module MrtDashboard
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-
+    config.global_id.app = 'mrt-dashboard'
+    
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- config/environments/docker.rb
+++ config/environments/docker.rb
@@ -11,7 +11,7 @@ MrtDashboard::Application.configure do
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
-  config.eager_load = true
+  config.eager_load = false
 
   config.active_support.deprecation = :log
 
