caf66343a38dca2b2921097cac69f870f9834ba4
Terry Brady <terrence.brady@ucop.edu>
2025-03-07 14:54:14 -0800

    simplify log override

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,11 +1,9 @@
 Rails.application.configure do
-  unless Rails.env.docker?
-    config.lograge.enabled = true
-    config.lograge.custom_options = ->(event) do
-      { time: event.time }
-    end
-    config.lograge.formatter = Lograge::Formatters::Logstash.new
-    config.lograge.keep_original_rails_log = true
-    config.lograge.logger = ActiveSupport::Logger.new "#{Rails.root}/log/lograge_#{Rails.env}.log"
+  config.lograge.enabled = true
+  config.lograge.custom_options = ->(event) do
+    { time: event.time }
   end
+  config.lograge.formatter = Lograge::Formatters::Logstash.new
+  config.lograge.keep_original_rails_log = true
+  config.lograge.logger = ActiveSupport::Logger.new "#{Rails.root}/log/lograge_#{Rails.env}.log"
 end
