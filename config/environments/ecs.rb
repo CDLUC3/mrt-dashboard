6f81f620d369fbf1708be911fd11c7a82a8d841d
Terry Brady <terrywbrady@gmail.com>
2025-04-29 08:36:42 -0700

    add ecs config

---
+++ config/environments/ecs.rb
@@ -0,0 +1,24 @@
+require 'active_record/errors'
+Rails.application.configure do
+  config.action_controller.perform_caching = true
+  config.autoload_paths                   += %W[#{config.root}/lib]
+  config.cache_classes                     = true
+  config.consider_all_requests_local       = false
+  config.i18n.fallbacks                    = true
+  config.serve_static_files = true
+
+  # Eager load code on boot. This eager loads most of Rails and
+  # your application in memory, allowing both threaded web servers
+  # and those relying on copy on write to perform better.
+  # Rake tasks automatically ignore this option for performance.
+  config.eager_load = false
+
+  config.active_support.deprecation = :log
+
+  # Prepend all log lines with the following tags.
+  config.log_tags = [:request_id]
+
+  logger = ActiveSupport::Logger.new($stdout)
+  logger.formatter = config.log_formatter
+  config.logger = ActiveSupport::TaggedLogging.new(logger)
+end
