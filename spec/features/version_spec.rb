a141fd7dc69bf1280077778d6d53d8c6c4f072b5
Terry Brady <terrywbrady@gmail.com>
2025-05-22 15:11:02 -0700

    improve testing on desktop

--- spec/features/version_spec.rb
+++ spec/features/version_spec.rb
@@ -151,8 +151,13 @@ describe 'versions', js: true do
     user_id = mock_user(name: 'Rachel Roe', password: password)
     mock_permissions_view_only(user_id, collection_1_id)
 
+    sleep 1
+
     log_out!
+    sleep 1
+
     log_in_with(user_id, password)
+    sleep 1
 
     index_path = url_for(
       controller: :version,
@@ -161,7 +166,10 @@ describe 'versions', js: true do
       version: version.number,
       only_path: true
     )
+    sleep 1
+
     visit(index_path)
+    sleep 1
 
     expect(page).not_to have_content('Download version')
     expect(page).to have_content('You do not have permission to download this object.')
