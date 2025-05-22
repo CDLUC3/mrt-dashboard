5eba3f1d542cc026d250a86f26b0634cd3d4c8ef
Terry Brady <terrywbrady@gmail.com>
2025-05-22 13:50:43 -0700

    fix test - choose coll

--- spec/features/collection_spec.rb
+++ spec/features/collection_spec.rb
@@ -175,12 +175,14 @@ describe 'collections' do
       log_in_with(LDAP_CONFIG['guest_user'], LDAP_CONFIG['guest_password'])
       allow(APP_CONFIG['redirects']).to receive(:fetch).with('collection_1_profile', '').and_return('https://cdlib.org/')
       visit(index_path)
+      visit(index_path) if current_url =~ /choose_collection$/
       expect(current_url).to eq('https://cdlib.org/')
     end
 
     it 'requires read permissions', js: true do
       log_in_with(user_id, password)
       visit(index_path)
+      visit(index_path) if current_url =~ /choose_collection$/
       expect(page).to have_content('Not authorized')
     end
 
