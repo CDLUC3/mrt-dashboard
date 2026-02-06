a4ede700ebea4bd0166662b80787cbeee22bac06
Terry Brady <terrywbrady@gmail.com>
2026-02-05 16:51:02 -0800

    try s3 storage for file uploads

--- config/environments/ecs.rb
+++ config/environments/ecs.rb
@@ -15,6 +15,9 @@ Rails.application.configure do
 
   config.active_support.deprecation = :log
 
+  # Store uploaded files on the local file system (see config/storage.yml for options).
+  config.active_storage.service = :amazon
+
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
 
