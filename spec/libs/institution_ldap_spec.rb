c1c36da0f584e3cd37e6c693c47153add1c4bab5
Terry Brady <terrywbrady@gmail.com>
2026-06-02 11:31:54 -0700

    ssl verify in rspec

--- spec/libs/institution_ldap_spec.rb
+++ spec/libs/institution_ldap_spec.rb
@@ -20,7 +20,8 @@ module InstitutionLdap
         encryption: {
           method: :simple_tls,
           tls_options: {
-            ssl_version: LDAP_CONFIG['tls']
+            ssl_version: LDAP_CONFIG['tls'],
+            verify_mode: OpenSSL::SSL::VERIFY_NONE
           }
         },
         connect_timeout: LDAP_CONFIG['connect_timeout']
